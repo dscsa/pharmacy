6a10c5493de0adf50bf43fe486599b0b8de07781
akircher <adam@Adams-MacBook-Pro.local>
2017-04-30 08:14:51 -0700

    Remove unnecessary code

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ var gsheet = "https://spreadsheets.google.com/feeds/list/1MV5mq6605X7U1Np2fpwZ1R
 //ovrg94l is the worksheet id.  To get this you have to use https://spreadsheets.google.com/feeds/worksheets/1MV5mq6605X7U1Np2fpwZ1RHkaCpjsb7YqieLQsEQK88/private/full
 var medications
 
-Cognito.load("forms", { id: "17" }, {success:function() {
-  console.log('cognito success')
-  setTimeout(load, 1000)
-}})
+Cognito.load("forms", { id: "17" }, {success:load})
 
 jQuery.ajax({
    url:gsheet,
@@ -14,6 +11,7 @@ jQuery.ajax({
    success:function($data) {
      console.log('$data.feed.entry', $data.feed.entry)
      medications = $data.feed.entry.map(gsheet2select)
+     console.log('medications', medications)
      load()
    }
 })
@@ -23,7 +21,6 @@ document.body.removeAttribute("id");
 
 load.count = 0
 function load() {
-  console.log('load.count', load.count)
   if(++load.count < 2) return
   showAcceptTerms()
   upgradeMedication()
@@ -33,23 +30,14 @@ function upgradeMedication() {
   var medicationSelect = jQuery('[data-field="SearchAndSelectMedicationsByGenericName"] select')
   var medicationPrice  = jQuery('[data-field="MedicationPrice"] select')
   var medicationList   = jQuery('[data-field="MedicationList"] input')
-console.log(1)
   medicationSelect.children().remove()
-  console.log(2)
-  medicationSelect.select2({multiple:true,data:medications})
-  .on("select2:open", toggleHeader)
-  .on("select2:close", toggleHeader)
-  .on("change", updatePrice)
+  medicationSelect.select2({multiple:true,data:medications}).on("change", updatePrice)
 
   function updatePrice(e) {
-    console.log(3)
     var price = medicationSelect.select2('data').reduce(sum, 0)
-  console.log(4)
     //We have to update a text box because cognito won't save values from a multi-select form
     medicationPrice.val(Math.min(100, price)).click().change()
-      console.log(5)
     medicationList.val(medicationSelect.val()).click().change()
-      console.log(6)
   }
 }
 
@@ -68,12 +56,6 @@ function gsheet2select(entry, i) {
   var drug = ' '+entry.gsx$drugname.$t+', $'+price+message
   var result = {id:drug, text:drug, disabled:entry.gsx$supplylevel.$t == 'Out of Stock', price:price}
   return result
-}
-
-function toggleHeader() {
-  console.log('A')
-  jQuery('header').toggle()
-  console.log('B')
 }
 
 function sum(a, b) {
