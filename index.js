08480b4a710cc41ec6684cc55d4b9f2eea2de7b8
akircher <adam@Adams-MacBook-Pro.local>
2017-05-04 13:29:21 -0700

    1st attempt at adding backup pharmacy choices

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var gsheet = "https://spreadsheets.google.com/feeds/list/1MV5mq6605X7U1Np2fpwZ1RHkaCpjsb7YqieLQsEQK88/ovrg94l/public/values?alt=json"
+var medicationGsheet = "https://spreadsheets.google.com/feeds/list/1MV5mq6605X7U1Np2fpwZ1RHkaCpjsb7YqieLQsEQK88/ovrg94l/public/values?alt=json"
+var pharmacyGsheet  = "https://docs.google.com/spreadsheets/d/11Ew_naOBwFihUrkaQnqVTn_3rEx6eAwMvGzksVTv_10/edit#gid=0"
 //ovrg94l is the worksheet id.  To get this you have to use https://spreadsheets.google.com/feeds/worksheets/1MV5mq6605X7U1Np2fpwZ1RHkaCpjsb7YqieLQsEQK88/private/full
 var medications
+var pharmacies
 
 Cognito.load("forms", { id: "17" }, {success:load})
 
@@ -12,13 +14,24 @@ function load() {
     ExoJQuery(document).on('afterNavigate.cognito', navigate)
 
     ExoJQuery.ajax({
-      url:gsheet,
+      url:medicationGsheet,
       type: 'GET',
       cache:true,
       success:function($data) {
-        console.log('$data.feed.entry', $data.feed.entry)
-        medications = $data.feed.entry.map(gsheet2select)
-        console.log('medications', medications)
+        console.log('medications gsheet', $data.feed.entry)
+        medications = $data.feed.entry.map(medication2select)
+        console.log('medications cleaned', medications)
+      }
+    })
+
+    ExoJQuery.ajax({
+      url:pharmacyGsheet,
+      type: 'GET',
+      cache:true,
+      success:function($data) {
+        console.log('medications gsheet', $data.feed.entry)
+        pharmacies = $data.feed.entry.map(pharmacy2select)
+        console.log('medications cleaned', pharmacies)
       }
     })
   })
@@ -51,7 +64,18 @@ function upgradeMedication() {
   }
 }
 
-function gsheet2select(entry, i) {
+function sum(a, b) {
+  console.log('a', a, 'b', b)
+  return +b.price+a
+}
+
+function showAcceptTerms() {
+  ExoJQuery('.loader').hide()
+  ExoJQuery('#c-submit-button').parent().parent().prepend('<div style="font-size:12px; max-width:785px; margin-left:10px; margin-bottom:10px; padding-top:10px">By clicking Accept & Submit, I attest to the statements below and understand that the medication(s) that I am receiving from SIRUM now & in the future may have been donated, previously dispensed, and potentially stored in an uncontrolled environment.</div>')
+}
+
+
+function medication2select(entry, i) {
   var price     = entry.gsx$day_2.$t || entry.gsx$day.$t
   var message   = []
 
@@ -64,16 +88,10 @@ function gsheet2select(entry, i) {
   message = message.length ? ' ('+message.join(', ')+')' : ''
 
   var drug = ' '+entry.gsx$drugname.$t+', '+price+message
-  var result = {id:drug, text:drug, disabled:entry.gsx$supplylevel.$t == 'Out of Stock', price:price.replace('$', '')}
-  return result
+  return {id:drug, text:drug, disabled:entry.gsx$supplylevel.$t == 'Out of Stock', price:price.replace('$', '')}
 }
 
-function sum(a, b) {
-  console.log('a', a, 'b', b)
-  return +b.price+a
-}
-
-function showAcceptTerms() {
-  ExoJQuery('.loader').hide()
-  ExoJQuery('#c-submit-button').parent().parent().prepend('<div style="font-size:12px; max-width:785px; margin-left:10px; margin-bottom:10px; padding-top:10px">By clicking Accept & Submit, I attest to the statements below and understand that the medication(s) that I am receiving from SIRUM now & in the future may have been donated, previously dispensed, and potentially stored in an uncontrolled environment.</div>')
+function pharmacy2select(entry, i) {
+  console.log('pharmacy entry')
+  return {id:drug, text:entry.gsx$name.$t+', '+entry.gsx$cleanaddress.$t+'(phone:'+entry.gsx$phone.$t+', fax:'+entry.gsx$fax.$t+')'}
 }
