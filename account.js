4e84d943b9f54148126800f635e1c6a7bbe5458f
akircher <adam@Adams-MacBook-Pro.local>
2017-05-13 17:55:40 -0400

    Override autofocus on billing name

--- account.js
+++ account.js
@@ -27,6 +27,7 @@ function load() {
     }
   })
 
+  jQuery("#medicationSelect").focus()
   jQuery('#billing_state').prop('disabled', true)
   jQuery('label#eRX').toggle()
   jQuery("input[name='source']").change(function($event){
