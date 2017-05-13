338ef973d64d70faf2ec266a80f74ff28ca87015
akircher <adam@Adams-MacBook-Pro.local>
2017-05-13 17:58:25 -0400

    Try different approach to removing autofocus

--- account.js
+++ account.js
@@ -27,7 +27,7 @@ function load() {
     }
   })
 
-  jQuery("#medicationSelect").focus()
+  jQuery("#billing_first_name").prop('autofocus', false)
   jQuery('#billing_state').prop('disabled', true)
   jQuery('label#eRX').toggle()
   jQuery("input[name='source']").change(function($event){
