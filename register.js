3768b6f6bb02e822de35cf5fbf91b557e4367f6d
akircher <adam@Adams-MacBook-Pro.local>
2017-05-14 10:56:02 -0400

    Try adding product on registration

--- register.js
+++ register.js
@@ -2,7 +2,14 @@ jQuery(load)
 
 function load() {
   jQuery('form').submit(function(e) {
-    this._wp_http_referer.value = "/account/orders?add-to-cart=281"
+    jQuery.ajax({
+      url:'/account?add-to-cart=281',
+      type: 'GET',
+      success:function($data) {
+        console.log('registering, add product', $data)
+      }
+    })
+    this._wp_http_referer.value = "/account/orders"
     this.sr_firstname.value = this.billing_first_name.value
     this.sr_lastname.value  = this.billing_last_name.value
     this.username.value     = this.sr_firstname.value+' '+this.sr_lastname.value
