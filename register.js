150b5c8f0bd81a5e2b6f03288949edea844bec14
akircher <adam@Adams-MacBook-Pro.local>
2017-05-14 03:56:38 -0400

    Try adding default values

--- register.js
+++ register.js
@@ -1,12 +1,9 @@
 jQuery(load)
 
 function load() {
-  jQuery('input[name="register"]').click(function(e) {
-    console.log('button', this)
-  })
-
   jQuery('form').submit(function(e) {
-    console.log('form', this.billing_first_name, this.billing_last_name, this.sr_firstname, this.sr_lastname, this.username)
-    e.preventDefault()
+    this.sr_firstname.value = this.billing_first_name.value
+    this.sr_lastname.value  = this.billing_last_name.value
+    this.username.value     = this.sr_firstname.value+' '+this.sr_lastname.value
   })
 }
