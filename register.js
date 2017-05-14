d510792aac08e3e548c8b5762f3478a4876fe92a
akircher <adam@Adams-MacBook-Pro.local>
2017-05-14 03:50:32 -0400

    More debugging

--- register.js
+++ register.js
@@ -3,7 +3,6 @@ jQuery(load)
 function load() {
   jQuery('input[name="register"]').click(function(e) {
     console.log('button', this)
-    e.preventDefault()
   })
 
   jQuery('form').submit(function(e) {
