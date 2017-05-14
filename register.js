4e246a3baece79dc605bddc8091ac058b0b80791
akircher <adam@Adams-MacBook-Pro.local>
2017-05-14 04:28:00 -0400

    Force redirect on registration

--- register.js
+++ register.js
@@ -1,7 +1,8 @@
 jQuery(load)
 
 function load() {
-  jQuery('form').attr('action', '/account/orders').submit(function(e) {
+  jQuery('form').submit(function(e) {
+    this._wp_http_referer.value = "/account/orders"
     this.sr_firstname.value = this.billing_first_name.value
     this.sr_lastname.value  = this.billing_last_name.value
     this.username.value     = this.sr_firstname.value+' '+this.sr_lastname.value
