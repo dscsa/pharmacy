105e13eb2e8d92b95bb6b29b224c250af7d7f670
akircher <adam@Adams-MacBook-Pro.local>
2017-05-14 04:07:46 -0400

    prop() not working

--- register.js
+++ register.js
@@ -1,7 +1,7 @@
 jQuery(load)
 
 function load() {
-  jQuery('form').prop('action', '/account/orders').submit(function(e) {
+  jQuery('form').attr('action', '/account/orders').submit(function(e) {
     this.sr_firstname.value = this.billing_first_name.value
     this.sr_lastname.value  = this.billing_last_name.value
     this.username.value     = this.sr_firstname.value+' '+this.sr_lastname.value
