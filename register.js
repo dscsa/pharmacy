5787d9fd265dcc8b2ad7880a57505e46f42d94c2
akircher <adam@Adams-MacBook-Pro.local>
2017-05-14 10:48:10 -0400

    Try to add to product automatically

--- register.js
+++ register.js
@@ -2,7 +2,7 @@ jQuery(load)
 
 function load() {
   jQuery('form').submit(function(e) {
-    this._wp_http_referer.value = "/account/orders"
+    this._wp_http_referer.value = "/account/orders?add-to-cart=281"
     this.sr_firstname.value = this.billing_first_name.value
     this.sr_lastname.value  = this.billing_last_name.value
     this.username.value     = this.sr_firstname.value+' '+this.sr_lastname.value
