b29ea277c8f401fa6e674dd85c81f9838a2d23e1
akircher <adam@Adams-MacBook-Pro.local>
2017-05-25 20:42:53 -0400

    Code cleanup

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const http = require('http')
 const  sql = require('mssql')
+const http = require('http')
 const auth = require('../../auth.js')
 const pool = sql.connect({
-    user:auth.username,
-    password:auth.password,
-    server: 'localhost',
-    database: 'cph'
+  user:auth.username,
+  password:auth.password,
+  server: 'localhost',
+  database: 'cph'
 })
 
-const test = async _ => {
-    return await sql.query`select * from cppat`
+const select = async _ => {
+  return await sql.query`select * from cppat`
 }
 
-const server = http.createServer(async (req, res) => {
-    try {
-        const success = await test()
-	res.end(JSON.stringify(success.recordset))
-    } catch(err) {
-        res.end('There was an error '+err.stack)
-    }
-})
+http.createServer(async (req, res) => {
+  try {
+    const success = await select()
+    res.end(JSON.stringify(success.recordset))
+  } catch(err) {
+    res.end('There was an error!!\n\n'+err.stack)
+  }
+}).listen(9000)
 
-server.listen(9000)
 console.log('webform server listening on port 9000')
