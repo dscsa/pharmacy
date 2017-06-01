3fef4701097f23dd6cf16323eca4f43787de9ad9
akircher <adam@Adams-MacBook-Pro.local>
2017-06-01 17:44:24 -0400

    More testing

--- woocommerce/test.php
+++ woocommerce/test.php
@@ -1,11 +1,9 @@
 <?php echo 'adam';
 
-try {
   echo '2';
   echo sqlsrv_connect('GOODPILL-SERVER', ['Database' => 'cph']);
   echo '3';
-} catch($err) {
   echo '4';
   echo $err;
   echo '5';
-} ?>
+?>
