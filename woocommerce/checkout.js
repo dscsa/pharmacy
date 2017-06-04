8c486aa1a69ce20f3e7185c2a3e6dc8ee534dbe2
akircher <adam@Adams-MacBook-Pro.local>
2017-06-04 14:14:51 -0400

    Can't hardwire selector to lang otherwise if language changes it won't fire

--- woocommerce/checkout.js
+++ woocommerce/checkout.js
@@ -20,7 +20,7 @@ function load() {
   setTimeout(showLang, 3000) //Both languages hide by default.  Need delay because ZIP, City/Town, Credit Card are delayed
   function showLang() { jQuery('.'+lang).show() }
 
-  jQuery("#account_allergies_"+lang).on('change', function(){
+  jQuery("#account_allergies_english:visible, #account_allergies_spanish:visible").on('change', function(){
     var children = jQuery(".checkbox, #"+this.id+"_other")
     console.log('children', children, this.value)
     this.value == 'No' ? children.hide() : children.show()
