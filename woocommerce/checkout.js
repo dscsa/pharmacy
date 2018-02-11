f89aef951f4f1194b2f3431806c9f5b40012c6c1
akircher <adam@Adams-MacBook-Pro.local>
2018-02-11 16:55:49 -0500

    Move some jQuery styling to CSS

--- woocommerce/checkout.js
+++ woocommerce/checkout.js
@@ -7,10 +7,6 @@ function load() {
   //Show our form fields at the beginning rather than the end
   jQuery('form.checkout .col-1').prepend(jQuery('.woocommerce-additional-fields'))
 
-  //Hide default dashboard content and show our order form instead
-  jQuery('form.checkout').show()
-  jQuery('.woocommerce-MyAccount-content').hide()
-
   //Move coupon from top of page to be next to payment instead
   jQuery( "a.showcoupon" ).parent().appendTo(jQuery('#customer_details'))
   jQuery('form.checkout_coupon').appendTo(jQuery('#customer_details'))
@@ -23,7 +19,7 @@ function load() {
   setTimeout(function() {
     jQuery('form.checkout').off('keydown', '.address-field input.input-text, .update_totals_on_change input.input-text')
   }, 1000)
-  
+
   //Toggle medication select and backup pharmacy text based on whether
   //Rx is being sent from doctor or transferred from a pharmacy.
   setSource()
