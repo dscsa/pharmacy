411fe6a85ab5425de49082cbbc62d56db95004aa
akircher <adam@Adams-MacBook-Pro.local>
2018-04-26 14:33:36 -0400

    Prevent date picker from bypassing readonly attribute

--- woocommerce/common.js
+++ woocommerce/common.js
@@ -74,7 +74,7 @@ function upgradePharmacy(pharmacies) {
 }
 
 function upgradeBirthdate() { //now 2 on same page (loing & register) so jquery id, #, selection doesn't work since assumes ids are unique
-  jQuery('[name=birth_date]').each(function() {
+  jQuery('[name=birth_date]:not([readonly])').each(function() {
     var elem = jQuery(this)
     elem.datepicker({changeMonth:true, changeYear:true, yearRange:"c-100:c", defaultDate:elem.val() || "-50y", dateFormat:"yy-mm-dd", constrainInput:false})
   })
