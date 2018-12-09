ed55243df0b5f1565cd6abcf5b2608896510bc47
akircher <adam@Adams-MacBook-Pro-2.local>
2018-12-08 23:46:50 -0500

    Add semicolons to return statements

--- amazon_ami.php
+++ amazon_ami.php
@@ -1774,7 +1774,7 @@ function add_patient($first_name, $last_name, $birth_date, $phone, $language) {
 // Procedure dbo.SirumWeb_AddToPatientComment (@PatID int, @CmtToAdd VARCHAR(4096)
 // The comment will be appended to the existing comment if it is not already in the comment field.
 function append_comment($guardian_id, $comment) {
-  if ( ! $guardian_id) return
+  if ( ! $guardian_id) return;
   $comment = str_replace("'", "''", $comment); //We need to escape single quotes in case comment has one
   return db_run("SirumWeb_AddToPatientComment '$guardian_id', '$comment'");
 }
@@ -1792,7 +1792,7 @@ function append_comment($guardian_id, $comment) {
 //   ,@PharmacyFaxNo varchar(20)   -- Phone Fax Number
 // If you send the NDC, it will use it.  If you do not send and NCD it will attempt to look up the drug by the name.  I am not sure that this will work correctly, the name you pass in would most likely have to be an exact match, even though I am using  like logic  (ie “%Aspirin 325mg% “) to search.  We may have to work on this a bit more
 function add_preorder($guardian_id, $drug_names, $pharmacy) {
-   if ( ! $guardian_id) return
+   if ( ! $guardian_id) return;
    $store = json_decode(stripslashes($pharmacy));
    $phone = cleanPhone($store->phone) ?: '0000000000';
    $fax = cleanPhone($store->fax) ?: '0000000000';
@@ -1830,7 +1830,7 @@ function add_preorder($guardian_id, $drug_names, $pharmacy) {
 // If you send the NDC, it will use it.  If you do not send and NCD it will attempt to look up the drug by the name.  I am not sure that this will work correctly, the name you pass in would most likely have to be an exact match, even though I am using  like logic  (ie “%Aspirin 325mg% “) to search.  We may have to work on this a bit more
 function update_pharmacy($guardian_id, $pharmacy) {
 
-  if ( ! $guardian_id) return
+  if ( ! $guardian_id) return;
 
   $store = json_decode(stripslashes($pharmacy));
 
@@ -1850,12 +1850,12 @@ function update_pharmacy($guardian_id, $pharmacy) {
 }
 
 function update_stripe_tokens($guardian_id, $value) {
-  if ( ! $guardian_id) return
+  if ( ! $guardian_id) return;
   return db_run("SirumWeb_AddUpdatePatientUD '$guardian_id', '3', '$value'");
 }
 
 function update_card_and_coupon($guardian_id, $card = [], $coupon) {
-  if ( ! $guardian_id) return
+  if ( ! $guardian_id) return;
   //Meet guardian 50 character limit
   //Last4 4, Month 2, Year 2, Type (Mastercard = 10), Delimiter 4, So coupon will be truncated if over 28 characters
   $value = $card['last4'].','.$card['month'].'/'.substr($card['year'] ?: '', 2).','.$card['type'].','.$coupon;
@@ -1866,7 +1866,7 @@ function update_card_and_coupon($guardian_id, $card = [], $coupon) {
 //Procedure dbo.SirumWeb_AddUpdatePatEmail (@PatID int, @EMailAddress VARCHAR(255)
 //Set the patID and the new email address
 function update_email($guardian_id, $email) {
-  if ( ! $guardian_id) return
+  if ( ! $guardian_id) return;
   return db_run("SirumWeb_AddUpdatePatEmail '$guardian_id', '$email'");
 }
 
@@ -1875,7 +1875,7 @@ function update_email($guardian_id, $email) {
 //If user has no explicit dates then PHP will set $_POST[autofill_resume] AND $_POST[$rx_autofill] to null rathern than array of empty keys.  So we have to use the rx_autofill_array instead.
 function update_autofill($guardian_id, $pat_autofill, $rx_autofill, $autofill_resume) {
 
-  if ( ! $guardian_id) return
+  if ( ! $guardian_id) return;
 
   $rx_autofill = $pat_autofill ? json_encode($rx_autofill ?: []) : '';
   $autofill_resume = json_encode($autofill_resume ?: []);
