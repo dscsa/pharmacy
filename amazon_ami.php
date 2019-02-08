512a007e5f2a47a5c1d427a7bd8c1932c746662f
akircher <adam@Adams-MacBook-Pro-2.local>
2019-02-08 12:20:56 -0500

    Remove Order Receipt Emails from webform for right now
    May conflict with Shopping Sheet Emails

--- amazon_ami.php
+++ amazon_ami.php
@@ -1319,6 +1319,39 @@ function dscsa_new_order($order_id) {
   }
 }
 
+/**
+ * Unhook and remove WooCommerce default emails.
+ */
+add_action( 'woocommerce_email', 'unhook_those_pesky_emails' );
+
+function unhook_those_pesky_emails( $email_class ) {
+
+		/**
+		 * Hooks for sending emails during store events
+		 **/
+		//remove_action( 'woocommerce_low_stock_notification', array( $email_class, 'low_stock' ) );
+		//remove_action( 'woocommerce_no_stock_notification', array( $email_class, 'no_stock' ) );
+		//remove_action( 'woocommerce_product_on_backorder_notification', array( $email_class, 'backorder' ) );
+
+		// New order emails
+		remove_action( 'woocommerce_order_status_pending_to_processing_notification', array( $email_class->emails['WC_Email_New_Order'], 'trigger' ) );
+		//remove_action( 'woocommerce_order_status_pending_to_completed_notification', array( $email_class->emails['WC_Email_New_Order'], 'trigger' ) );
+		//remove_action( 'woocommerce_order_status_pending_to_on-hold_notification', array( $email_class->emails['WC_Email_New_Order'], 'trigger' ) );
+		//remove_action( 'woocommerce_order_status_failed_to_processing_notification', array( $email_class->emails['WC_Email_New_Order'], 'trigger' ) );
+		//remove_action( 'woocommerce_order_status_failed_to_completed_notification', array( $email_class->emails['WC_Email_New_Order'], 'trigger' ) );
+		//remove_action( 'woocommerce_order_status_failed_to_on-hold_notification', array( $email_class->emails['WC_Email_New_Order'], 'trigger' ) );
+
+		// Processing order emails
+		remove_action( 'woocommerce_order_status_pending_to_processing_notification', array( $email_class->emails['WC_Email_Customer_Processing_Order'], 'trigger' ) );
+		//remove_action( 'woocommerce_order_status_pending_to_on-hold_notification', array( $email_class->emails['WC_Email_Customer_Processing_Order'], 'trigger' ) );
+
+		// Completed order emails
+		//remove_action( 'woocommerce_order_status_completed_notification', array( $email_class->emails['WC_Email_Customer_Completed_Order'], 'trigger' ) );
+
+		// Note emails
+		//remove_action( 'woocommerce_new_customer_note_notification', array( $email_class->emails['WC_Email_Customer_Note'], 'trigger' ) );
+}
+
 add_filter( 'wc_order_statuses', 'dscsa_renaming_order_status' );
 function dscsa_renaming_order_status( $order_statuses ) {
     $order_statuses['wc-processing'] = _x('Received Rx', 'Order status', 'woocommerce');
