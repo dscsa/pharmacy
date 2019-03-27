833984d8390fb4aad9ffb03b94451fe8107bc83b
akircher <adam@Adams-MacBook-Pro-2.local>
2019-03-27 19:54:50 -0400

    Try a new selector

--- woocommerce/checkout.js
+++ woocommerce/checkout.js
@@ -9,9 +9,10 @@ function load() {
   jQuery('form.checkout .col-1').prepend(jQuery('.woocommerce-additional-fields'))
 
   //Move coupon from top of page to be next to payment instead and set width back to 100%
+  jQuery('div.woocommerce-form-coupon-toggle').appendTo(jQuery('#customer_details')).addClass('reset-to-full-width')
   //jQuery('a.showcoupon').parent().appendTo(jQuery('#customer_details'))
   //jQuery('.woocommerce-info').addClass('reset-to-full-width') //includes the above selector but is more broad to match checkout.css
-  //jQuery('form.checkout_coupon').appendTo(jQuery('#customer_details')).addClass('reset-to-full-width')
+  jQuery('form.checkout_coupon').appendTo(jQuery('#customer_details')).addClass('reset-to-full-width')
 
   //This class causes update_checkout_totals, which causes a spinner and delay that we don't need
   //https://github.com/woocommerce/woocommerce/blob/master/assets/js/frontend/checkout.js
