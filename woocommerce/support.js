43e461f1c52b787c2e45fafec5ac2d655ff93be2
akircher <adam@Adams-MacBook-Pro-2.local>
2019-06-29 19:59:08 -0400

    Step 2 - more fixes

--- woocommerce/support.js
+++ woocommerce/support.js
@@ -18,11 +18,11 @@ function load() {
 
   jQuery('#wpas_call-type').on("select2:selecting", function(e) {
      console.log('support.js selecting', e.params.args.data)
-     jQuery('#wpas_'+ map[e.params.args.data.text]).show()
+     jQuery('#wpas_'+ map[e.params.args.data.text]).parent().show()
   });
 
   jQuery('#wpas_call-type').on("select2:unselecting", function(e) {
     console.log('support.js unselecting', e.params.args.data)
-     jQuery('#wpas_'+ map[e.params.args.data.text]).hide()
+     jQuery('#wpas_'+ map[e.params.args.data.text]).parent().hide()
   });
 }
