c45958431f4c6ef4f9d1d31f4431a8a8d80bcdc4
akircher <adam@Adams-MacBook-Pro-2.local>
2019-06-29 19:44:57 -0400

    More logging

--- amazon_ami.php
+++ amazon_ami.php
@@ -166,8 +166,8 @@ function dscsa_admin_scripts() {
   }
 
   if ($_GET['post_type'] == 'ticket') {
-    wp_enqueue_style('dscsa-support', 'https://dscsa.github.io/webform/woocommerce/support.css');
-    wp_enqueue_script('dscsa-support', 'https://dscsa.github.io/webform/woocommerce/support.js', ['jquery', 'dscsa-common']);
+    wp_enqueue_style('dscsa-support-css', 'https://dscsa.github.io/webform/woocommerce/support.css');
+    wp_enqueue_script('dscsa-support-js', 'https://dscsa.github.io/webform/woocommerce/support.js', ['jquery', 'dscsa-common']);
   }
 }
 
--- woocommerce/support.js
+++ woocommerce/support.js
@@ -1,6 +1,8 @@
 jQuery(load)
 function load() {
 
+  console.log('loading support.js')
+
   var map = {
    'General Info':'info',
    'Rx Info':'rx-info',
@@ -15,10 +17,12 @@ function load() {
 
 
   jQuery('#wpas_call-type').on("select2:selecting", function(e) {
+     console.log('support.js selecting', e.params.args.data)
      jQuery('#wpas_'+ map[e.params.args.data.text]).show()
   });
 
   jQuery('#wpas_call-type').on("select2:unselecting", function(e) {
+    console.log('support.js unselecting', e.params.args.data)
      jQuery('#wpas_'+ map[e.params.args.data.text]).hide()
   });
 }
