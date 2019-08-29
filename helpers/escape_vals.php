ff160187faefce38f7ce732367ad834a782ce307
akircher <adam@Adams-MacBook-Pro-2.local>
2019-08-28 21:43:15 -0400

    Still having problems with total_fills being NULL

--- helpers/escape_vals.php
+++ helpers/escape_vals.php
@@ -6,7 +6,7 @@ function escape_vals($rows) {
   $results = [];
   foreach( $rows as $row ) {
     foreach( $row as $key => $val ) {
-      $row[$key] = empty($val) ? 'NULL' : '"'.mysql_real_escape_string(trim($val)).'"'; //empty converts empty string or null to null
+      $row[$key] = $val === '' ? 'NULL' : '"'.mysql_real_escape_string(trim($val)).'"'; // convert empty string to null
     }
     $results[] = '('.implode(', ', $row).')';
   }
