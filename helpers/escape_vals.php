efce51bcc13a96111bff9dc6db69dd36781d4c68
akircher <adam@Adams-MacBook-Pro-2.local>
2019-08-28 21:23:28 -0400

    Problem was with escape function not the query

--- helpers/escape_vals.php
+++ helpers/escape_vals.php
@@ -6,7 +6,7 @@ function escape_vals($rows) {
   $results = [];
   foreach( $rows as $row ) {
     foreach( $row as $key => $val ) {
-      $row[$key] = $val ? '"'.mysql_real_escape_string(trim($val)).'"' : 'NULL';
+      $row[$key] = empty($val) ? 'NULL' : '"'.mysql_real_escape_string(trim($val)).'"'; //empty converts empty string or null to null
     }
     $results[] = '('.implode(', ', $row).')';
   }
