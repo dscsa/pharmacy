41c8ed8ed82f4b70fa37a3e7a0ad03fbbd1dfa5b
akircher <adam@Adams-MacBook-Pro-2.local>
2019-08-28 21:05:50 -0400

    Test a new theory

--- helpers/replace_empty_with_null.php
+++ helpers/replace_empty_with_null.php
@@ -1,12 +1,12 @@
 <?php
 
-function replace_empty_with_null($rows, $fields) {
+function replace_empty_with_null($rows) {
 
   //https://stackoverflow.com/questions/16710800/implode-data-from-a-multi-dimensional-array
   $results = [];
   foreach( $rows as $row ) {
-    foreach( $fields as $field) {
-      $row[$field] = $row[$field] ? '"'.$row[$field].'"' : 'NULL';
+    foreach( $row as $key => $val ) {
+      $row[$key] = $val ? '"'.mysql_real_escape_string(trim($val)).'"' : 'NULL';
     }
     $results[] = '('.implode(', ', $row).')';
   }
