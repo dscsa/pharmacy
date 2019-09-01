e9c648dde164c8a396c2d5e3b4d3fcd5a01cf635
akircher <adam@Adams-MacBook-Pro-2.local>
2019-09-01 16:03:10 -0400

    Change order so we can debug quicker

--- cron_jobs.php
+++ cron_jobs.php
@@ -1,24 +1,26 @@
 <?php
-
+require_once 'imports/import_cp_order_items.php';
 require_once 'imports/import_v2_drugs.php';
 require_once 'imports/import_v2_stock_by_month.php';
 require_once 'imports/import_cp_patients.php';
 require_once 'imports/import_cp_rxs_single.php';
 require_once 'imports/import_cp_orders.php';
-require_once 'imports/import_cp_order_items.php';
 
+require_once 'updates/update_order_items.php';
 require_once 'updates/update_drugs.php';
 require_once 'updates/update_stock_by_month.php';
 require_once 'updates/update_patients.php';
 require_once 'updates/update_rxs_single.php';
 require_once 'updates/update_orders.php';
-require_once 'updates/update_order_items.php';
 
 date_default_timezone_set('America/New_York');
 
 timer("", $time);
 
 //Imports
+import_cp_order_items();
+echo timer("import_cp_order_items", $time);
+
 import_v2_drugs();
 echo timer("import_v2_drugs", $time);
 
@@ -34,10 +36,10 @@ echo timer("import_cp_rxs_single", $time);
 import_cp_orders();
 echo timer("import_cp_orders", $time);
 
-import_cp_order_items();
-echo timer("import_cp_order_items", $time);
-
 //Updates
+update_order_items();
+echo timer("update_order_items", $time);
+
 update_drugs();
 echo timer("update_drugs", $time);
 
@@ -52,9 +54,6 @@ echo timer("update_rxs_single", $time);
 
 update_orders();
 echo timer("update_orders", $time);
-
-update_order_items();
-echo timer("update_order_items", $time);
 
 function timer($label, &$start) {
   $start ?: microtime(true);
