1b3b02fde91aca57e3e2824d8bfe832d22e6bf0f
akircher <adam@Adams-MacBook-Pro-2.local>
2019-09-26 20:50:51 -0400

    MAX DOESN'T WORK WITH TXT

--- imports/import_cp_patients.php
+++ imports/import_cp_patients.php
@@ -16,7 +16,7 @@ function import_cp_patients() {
       MAX(fname) as first_name,
       MAX(lname) as last_name,
       CONVERT(varchar, MAX(birth_date), 20) as birth_date,
-      MAX(pat.cmt) as patient_note,
+      MAX(CAST(pat.cmt as VARCHAR(MAX))) as patient_note, -- MAX DOESN'T WORK WITH TXT
 
       MAX(CONCAT(ph1.area_code, ph1.phone_no)) as phone1,
       MAX(CONCAT(ph2.area_code, ph2.phone_no)) as phone2,
