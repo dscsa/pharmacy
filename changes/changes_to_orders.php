ec4be79bf119e9f659bbbb9fb10fdf5c6136e902
akircher <adam@Adams-MacBook-Pro-2.local>
2019-10-26 19:24:13 -0400

    Try moving comments

--- changes/changes_to_orders.php
+++ changes/changes_to_orders.php
@@ -14,7 +14,6 @@ function changes_to_orders($new) {
     NOT old.order_source <=> new.order_source OR
     NOT old.order_stage <=> new.order_stage OR
     NOT old.order_status <=> new.order_status OR
-    -- Not in CP -- NOT old.invoice_doc_id <=> new.invoice_doc_id OR
     NOT old.order_address1 <=> new.order_address1 OR
     NOT old.order_address2 <=> new.order_address2 OR
     NOT old.order_city <=> new.order_city OR
@@ -24,7 +23,8 @@ function changes_to_orders($new) {
     NOT old.order_date_added <=> new.order_date_added OR
     NOT old.order_date_dispensed <=> new.order_date_dispensed OR
     NOT old.order_date_shipped <=> new.order_date_shipped
-    -- False Positives -- NOT old.order_date_changed <=> new.order_date_changed
+    -- Not in CP -- NOT old.invoice_doc_id <=> new.invoice_doc_id OR
+    -- False Positives -- NOT old.order_date_changed <=> new.order_date_changed OR
     -- Not in CP -- NOT old.order_date_returned <=> new.order_date_returned
   ";
 
