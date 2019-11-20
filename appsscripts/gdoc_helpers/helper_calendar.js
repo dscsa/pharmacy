c3ad4cb1d16ccda75db1aaf920b05572e2a0d8e2
akircher <adam@Adams-MacBook-Pro-2.local>
2019-11-20 16:41:27 -0500

    Date formatting issue

--- appsscripts/gdoc_helpers/helper_calendar.js
+++ appsscripts/gdoc_helpers/helper_calendar.js
@@ -1,5 +1,5 @@
 function createCalendarEvent(event) {
-  event.stop = addHours(event.hours, event.start).toJSON().slice(0, 18)
+  event.stop = addHours(event.hours, event.start).toJSON().slice(0, 19)
   CalendarApp.getCalendarById(event.cal_id).createEvent(event.title, event.start, event.stop, {description:event.description})
 }
 
