00ddfa20b96dbab8cff0e70b1c9d18d2e3ca0131
akircher <adam@Adams-MacBook-Pro-2.local>
2019-11-21 16:04:55 -0500

    More logging on pick lists

--- helpers/helper_v2.php
+++ helpers/helper_v2.php
@@ -13,5 +13,6 @@ function v2_fetch($url, $method = 'GET', $content = []) {
   ]);
 
   $response = file_get_contents(V2_IP.$url, false, $context);
+  email('v2_fetch', V2_IP.$url, $context, $response);
   return json_decode($response, true);
 }
