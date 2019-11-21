7d35b56813fb3434272a10de164e4bad70a74f27
akircher <adam@Adams-MacBook-Pro-2.local>
2019-11-21 18:11:48 -0500

    Try url encoding the start and end key arrays

--- exports/export_v2_order_items.php
+++ exports/export_v2_order_items.php
@@ -79,8 +79,8 @@ function make_pick_list($item) {
   $min_exp   = explode('-', date('Y-m', strtotime("+".($min_days-2*7)." days"))); //Used to use +14 days rather than -14 days as a buffer for dispensing and shipping. But since lots of prepacks expiring I am going to let almost expired things be prepacked
   $long_exp  = explode('-', date('Y-m', strtotime("+".($min_days+6*7)." days"))); //2015-05-13 We want any surplus from packing fast movers to be usable for ~6 weeks.  Otherwise a lot of prepacks expire on the shelf
 
-  $start_key = '["8889875187","month","'.$min_exp[0].'","'.$min_exp[1].'","'.$generic.'"]';
-  $end_key   = '["8889875187","month","'.$min_exp[0].'","'.$min_exp[1].'","'.$generic.'",{}]';
+  $start_key = rawurlencode('["8889875187","month","'.$min_exp[0].'","'.$min_exp[1].'","'.$generic.'"]');
+  $end_key   = rawurlencode('["8889875187","month","'.$min_exp[0].'","'.$min_exp[1].'","'.$generic.'",{}]');
 
   $url  = '/transaction/_design/inventory.qty-by-generic/_view/inventory.qty-by-generic?reduce=false&include_docs=true&limit=300&startkey='.$start_key.'&endkey='.$end_key;
 
