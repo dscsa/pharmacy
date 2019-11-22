8e25377314b37770106a98e13ba45eb531566d9d
akircher <adam@Adams-MacBook-Pro-2.local>
2019-11-21 20:31:41 -0500

    Solved v2_fetch issue with URL encoding double quotes

--- helpers/helper_v2.php
+++ helpers/helper_v2.php
@@ -19,6 +19,6 @@ function v2_fetch($url, $method = 'GET', $content = []) {
   $context = stream_context_create($opts);
 
   $response = file_get_contents(V2_IP.$url, false, $context);
-  email('v2_fetch', V2_IP.$url, $opts, $response, $http_response_header);
+  //email('v2_fetch', V2_IP.$url, $opts, $response, $http_response_header);
   return json_decode($response, true);
 }
