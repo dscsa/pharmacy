13c62a01e60d2b8bed74aea219471031859b1418
akircher <adam@Adams-MacBook-Pro-2.local>
2019-11-26 13:59:09 -0500

    php does not pass foreach args by reference

--- exports/export_gd_orders.php
+++ exports/export_gd_orders.php
@@ -12,12 +12,12 @@ function export_gd_update_invoice($order) {
   if ( ! count($order)) return;
 
   //Consolidate default and actual suffixes to avoid conditional overload in the invoice template
-  foreach($order as $item) {
-    $item['item_message_text'] = $item['item_message_text'] ?: ''; //Get rid of NULL
-    $item['days_dispensed'] = $item['days_dispensed_actual'] ?: $item['days_dispensed_default'];
-    $item['qty_dispensed'] = (float) $item['qty_dispensed_actual'] ?: $item['qty_dispensed_default']; //cast to float to get rid of .000 decimal
-    $item['refills_total'] = $item['refills_total_actual'] ?: $item['refills_total_default'];
-    $item['price_dispensed'] = $item['price_dispensed_actual'] ?: $item['price_dispensed_default'];
+  foreach($order as $i => $item) {
+    $order[$i]['item_message_text'] = $item['item_message_text'] ?: ''; //Get rid of NULL
+    $order[$i]['days_dispensed'] = $item['days_dispensed_actual'] ?: $item['days_dispensed_default'];
+    $order[$i]['qty_dispensed'] = (float) $item['qty_dispensed_actual'] ?: $item['qty_dispensed_default']; //cast to float to get rid of .000 decimal
+    $order[$i]['refills_total'] = $item['refills_total_actual'] ?: $item['refills_total_default'];
+    $order[$i]['price_dispensed'] = $item['price_dispensed_actual'] ?: $item['price_dispensed_default'];
   }
 
   $args = [
