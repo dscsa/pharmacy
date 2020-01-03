8cb175b004f32bad5f1144fa3d1a895fefb70d6b
akircher <adam@Adams-MacBook-Pro-2.local>
2020-01-02 19:33:35 -0500

    Extract full_order functions into a separate file

--- updates/update_orders.php
+++ updates/update_orders.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'changes/changes_to_orders.php';
+require_once 'helpers/helper_full_order.php';
 require_once 'helpers/helper_payment.php';
 require_once 'helpers/helper_syncing.php';
 require_once 'helpers/helper_communications.php';
@@ -19,69 +20,6 @@ function update_orders() {
   log_info("update_orders: $count_deleted deleted, $count_created created, $count_updated updated.", get_defined_vars());
 
   $mysql = new Mysql_Wc();
-
-  //order created -> add any additional rxs to order -> import order items -> sync all drugs in order
-
-  function get_full_order($order, $mysql) {
-
-    //gp_orders.invoice_number and other fields at end because otherwise potentially null gp_order_items.invoice_number will override gp_orders.invoice_number
-    $sql = "
-      SELECT
-        *,
-        gp_orders.invoice_number,
-        gp_rxs_grouped.* -- Need to put this first based on how we are joining, but make sure these grouped fields overwrite their single equivalents
-      FROM
-        gp_orders
-      JOIN gp_patients ON
-        gp_patients.patient_id_cp = gp_orders.patient_id_cp
-      LEFT JOIN gp_rxs_grouped ON -- Show all Rxs on Invoice regardless if they are in order or not
-        gp_rxs_grouped.patient_id_cp = gp_orders.patient_id_cp
-      LEFT JOIN gp_order_items ON
-        gp_order_items.invoice_number = gp_orders.invoice_number AND rx_numbers LIKE CONCAT('%,', gp_order_items.rx_number, ',%') -- In case the rx is added in a different orders
-      LEFT JOIN gp_rxs_single ON -- Needed to know qty_left for sync-to-date
-        gp_order_items.rx_number = gp_rxs_single.rx_number
-      LEFT JOIN gp_stock_live ON -- might not have a match if no GSN match
-        gp_rxs_grouped.drug_generic = gp_stock_live.drug_generic -- this is for the helper_days_dispensed msgs for unordered drugs
-      WHERE
-        gp_orders.invoice_number = $order[invoice_number]
-    ";
-
-    $order = $mysql->run($sql)[0];
-
-    if ( ! $order OR ! $order[0]['invoice_number']) {
-      log_error('ERROR! get_full_order: no invoice number', get_defined_vars());
-    } else {
-      //Consolidate default and actual suffixes to avoid conditional overload in the invoice template and redundant code within communications
-      foreach($order as $i => $item) {
-        $order[$i]['drug'] = $item['drug_name'] ?: $item['drug_generic'];
-        $order[$i]['days_dispensed'] = $item['days_dispensed_actual'] ?: $item['days_dispensed_default'];
-
-        if ( ! $item['item_date_added']) { //if not syncing to order lets provide a reason why we are not filling
-          $message = get_days_default($item)[1];
-          $order[$i]['item_message_key']  = array_search($message, RX_MESSAGE);
-          $order[$i]['item_message_text'] = message_text($message, $item);
-        }
-
-        $deduct_refill = $order[$i]['days_dispensed'] ? 1 : 0; //We want invoice to show refills after they are dispensed assuming we dispense items currently in order
-
-        $order[$i]['qty_dispensed'] = (float) ($item['qty_dispensed_actual'] ?: $item['qty_dispensed_default']); //cast to float to get rid of .000 decimal
-        $order[$i]['refills_total'] = (float) ($item['refills_total_actual'] ?: $item['refills_total_default'] - $deduct_refill);
-        $order[$i]['price_dispensed'] = (float) ($item['price_dispensed_actual'] ?: ($item['price_dispensed_default'] ?: 0));
-      }
-
-      usort($order, 'sort_order_by_day');
-    }
-
-    //log_info('get_full_order', get_defined_vars());
-
-    return $order;
-  }
-
-  function sort_order_by_day($a, $b) {
-    if ($b['days_dispensed'] > 0 AND $a['days_dispensed'] == 0) return 1;
-    if ($a['days_dispensed'] > 0 AND $b['days_dispensed'] == 0) return -1;
-    return strcmp($a['item_message_text'].$a['drug'], $b['item_message_text'].$b['drug']);
-  }
 
   //If just added to CP Order we need to
   //  - Find out any other rxs need to be added
