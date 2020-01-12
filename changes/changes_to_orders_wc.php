76eaa8b2951750240132aab0128fe4b92a450991
akircher <adam@Adams-MacBook-Pro-2.local>
2020-01-11 19:05:52 -0500

    Finally import seems to be working?

--- changes/changes_to_orders_wc.php
+++ changes/changes_to_orders_wc.php
@@ -20,8 +20,6 @@ function changes_to_orders_wc($new) {
   // 1st Result Set -> 1st Row -> 1st Column
   $columns = $mysql->run(get_column_names($new))[0][0]['columns'];
 
-  print_r($columns);
-
   //Get Deleted
   $deleted = $mysql->run(get_deleted_sql($new, $old, $id));
 
