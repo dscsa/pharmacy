7b87cc56af212b4c1caee4d41c710e0c8ded5b69
akircher <adam@Adams-MacBook-Pro-2.local>
2020-01-13 21:30:59 -0500

    One time update of Rxs Single

--- cronjobs/syncing.php
+++ cronjobs/syncing.php
@@ -35,22 +35,22 @@ $email .= timer("import_v2_drugs", $time);
 
 import_v2_stock_by_month();
 $email .= timer("import_v2_stock_by_month", $time);
-
+*/
 import_cp_rxs_single();
 $email .= timer("import_cp_rxs_single", $time);
 
+/*
 import_cp_patients();
 $email .= timer("import_cp_patients", $time);
 
 import_cp_order_items();
 $email .= timer("import_cp_order_items", $time);
-*/
 
-import_wc_orders();
-$email .= timer("import_wc_orders", $time);
+//import_wc_orders();
+//$email .= timer("import_wc_orders", $time);
 
-import_cp_orders();
-$email .= timer("import_cp_orders", $time);
+//import_cp_orders();
+//$email .= timer("import_cp_orders", $time);
 
 //Updates
 /*
@@ -59,16 +59,16 @@ $email .= timer("update_drugs", $time);
 
 update_stock_by_month();
 $email .= timer("update_stock_by_month", $time);
-
+*/
 update_rxs_single();
 $email .= timer("update_rxs_single", $time);
-
+/*
 update_patients();
 $email .= timer("update_patients", $time);
 
 update_order_items();
 $email .= timer("update_order_items", $time);
-*/
+
 update_orders_wc();
 $email .= timer("update_orders_wc", $time);
 
@@ -77,6 +77,8 @@ $email .= timer("update_orders_cp", $time);
 
 watch_invoices();
 $email .= timer("watch_invoices", $time);
+
+*/
 
 if ($email) {
   log_info("WebForm CRON Finished", get_defined_vars());
