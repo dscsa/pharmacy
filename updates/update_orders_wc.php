9f172d8a836633361932fbe628ad6cd1ac2cc953
akircher <adam@Adams-MacBook-Pro-2.local>
2020-01-15 14:15:26 -0500

    Try better classifying the different types of WC changes

--- updates/update_orders_wc.php
+++ updates/update_orders_wc.php
@@ -56,7 +56,13 @@ function update_orders_wc() {
   //2) An order is in CP but not in (never added to) WC, probably because of a tech bug.
   foreach($changes['deleted'] as $deleted) {
 
-    if ($deleted['invoice_number'] < 25305) {
+    if ($deleted['order_stage_wc'] == 'trashed') {
+      $notices[] = ["Order deleted in WC", $deleted];
+    } else {
+      $notices[] = ["Order never added to WC", $deleted];
+    }
+
+    if (false AND $deleted['invoice_number'] < 25305) {
 
       $order = get_full_order($deleted, $mysql);
 
@@ -70,8 +76,6 @@ function update_orders_wc() {
 
       //$notices[] = ["Will Add Order to WC", $deleted];
 
-    } else {
-      //$notices[] = ["Order deleted in WC", $deleted];
     }
 
   }
