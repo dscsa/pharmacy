9967ffbd7564a21dbacedd3005f555da5d04406b
akircher <adam@Adams-MacBook-Pro-2.local>
2020-01-15 19:31:29 -0500

    Refine logging again

--- updates/update_orders_wc.php
+++ updates/update_orders_wc.php
@@ -18,26 +18,6 @@ function update_orders_wc() {
   $mssql = new Mssql_Cp();
   $mysql = new Mysql_Wc();
 
-  function save_guardian_order($patient_id_cp, $source, $is_registered, $comment = '') {
-
-    $comment = str_replace("'", "''", $comment);
-    // Categories can be found or added select * From csct_code where ct_id=5007, UPDATE csct_code SET code_num=2, code=2, user_owned_yn = 1 WHERE code_id = 100824
-    // 0 Unspecified, 1 Webform Complete, 2 Webform eRx, 3 Webform Transfer, 6 Webform Refill, 7 Webform eRX with Note, 8 Webform Transfer with Note, 9 Webform Refill with Note,
-
-    if ($source == 'pharmacy')
-      $category = $comment ? 8 : 3;
-    else if ($source == 'erx' AND $is_registered)
-      $category = $comment ? 9 : 6;
-    else if ($source == 'erx'AND ! $is_registered)
-      $category = $comment ? 7 : 2;
-    else
-      $category = 0;
-
-    $result = $mssql->run("SirumWeb_AddFindOrder '$patient_id_cp', '$category', '$comment'");
-
-    return $result;
-  }
-
   $notices = [];
 
   //This captures 2 USE CASES:
@@ -76,7 +56,12 @@ function update_orders_wc() {
   //2) An order is in CP but not in (never added to) WC, probably because of a tech bug.
   foreach($changes['deleted'] as $deleted) {
 
-    if ($deleted['invoice_number'] < 25305) {
+
+    if ($deleted['order_stage_wc'] == 'trash') {
+
+      $notices[] = ["Order deleted in WC", $deleted];
+
+    } else {
 
       $order = get_full_order($deleted, $mysql);
 
@@ -86,35 +71,27 @@ function update_orders_wc() {
 
       export_wc_create_order($order);
       export_gd_publish_invoice($order);
-      $notices[] = ["Adding Order to WC", $order[0]['count_filled'], $deleted, $order];
 
-      //$notices[] = ["Will Add Order to WC", $deleted];
-
+      //$notices[] = ["Adding Order to WC", $order[0]['count_filled'], $deleted];
     }
-    else if ($deleted['order_stage_wc'] == 'trashed') {
-
-      $notices[] = ["Order deleted in WC", $deleted];
-
-    } else if ($deleted['tracking_number']) {
-
-      $notices[] = ["Historic Order never added to WC", $deleted];
-
-    } else {
-
-      $notices[] = ["New Order not added to WC yet", $deleted];
-
-    }
-
-
 
   }
 
   foreach($changes['updated'] as $updated) {
 
+
+
     if ($updated['order_stage_wc'] != $updated['old_order_stage_wc']) {
-      //$notices[] = ["WC Order Stage Change", $updated];
+
+      if ($deleted['order_stage_wc'] == 'trash')
+          $notices[] = ["Order trashed in WC", $deleted];
+      //else
+          //$notices[] = ["WC Order Stage Change", $updated];
+
     } else {
+
       $notices[] = ["Order updated in WC", $updated];
+
     }
 
   }
