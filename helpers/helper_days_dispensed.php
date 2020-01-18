4b44fa90608a3beac572a8e94a1dbe55ed248dfd
akircher <adam@Adams-MacBook-Pro-2.local>
2020-01-18 17:40:15 -0500

    Remove extra AND

--- helpers/helper_days_dispensed.php
+++ helpers/helper_days_dispensed.php
@@ -232,7 +232,7 @@ function sync_to_order_past_due($item) {
 }
 
 function sync_to_order_due_soon($item) {
-  return ! $item['item_date_added'] AND $item['refills_total'] >= 0.1 AND AND $item['refill_date_next'] AND (strtotime($item['refill_date_next'])  - strtotime($item['order_date_added'])) <= 15*24*60*60;
+  return ! $item['item_date_added'] AND $item['refills_total'] >= 0.1 AND $item['refill_date_next'] AND (strtotime($item['refill_date_next'])  - strtotime($item['order_date_added'])) <= 15*24*60*60;
 }
 
 function days_left_in_rx($item, $days_std = 90) {
