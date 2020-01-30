a94e61144ce60b89bf574a3fa2239f10da0929f6
akircher <adam@Adams-MacBook-Pro-2.local>
2020-01-29 23:20:05 -0500

    Remove 's

--- updates/update_patients_wc.php
+++ updates/update_patients_wc.php
@@ -298,7 +298,7 @@ function update_patients_wc() {
           if ($updated['phone1'] == $updated['phone2'] AND $updated['old_phone2']) {
             $sql = "UPDATE wp_usermeta SET meta_value = '$updated[old_phone2]' WHERE user_id = $updated[patient_id_wc] AND meta_key = 'billing_phone'";
             echo "
-            UPDATING phone2 within WC $updated[first_name] $updated[last_name] $updated[birth_date] $updated['phone1'] $key $changed[$key] $sql";
+            UPDATING phone2 within WC $updated[first_name] $updated[last_name] $updated[birth_date] $updated[phone1] $key $changed[$key] $sql";
             $mysql->run($sql);
           }
           else if ($updated['phone1'] == $updated['phone2'] AND ! $updated['old_phone2']) {
