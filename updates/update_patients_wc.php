4a48e49eda8dde913e3dbbead6f0f1dd5b3bd380
akircher <adam@Adams-MacBook-Pro-2.local>
2020-01-29 22:05:20 -0500

    More updates for malformed phone1

--- updates/update_patients_wc.php
+++ updates/update_patients_wc.php
@@ -272,12 +272,20 @@ function update_patients_wc() {
           $mysql->run($sql);
         }
 
-        if ($SirumWeb_AddUpdatePatHomePhone && $key == 'phone1' && (strlen($updated['phone1']) >= 10)) {
+        if ($SirumWeb_AddUpdatePatHomePhone && $key == 'phone1') {
           $SirumWeb_AddUpdatePatHomePhone = false;
 
-          $mssql->run("SirumWeb_AddUpdatePatHomePhone '$updated[patient_id_cp]', '$updated[phone1]'");
-          echo "
-          RAN $updated[first_name] $updated[last_name] $updated[birth_date] SirumWeb_AddUpdatePatHomePhone '$updated[patient_id_cp]', '$updated[phone1]'";
+          if (strlen($updated['phone1']) >= 10) {
+            $mssql->run("SirumWeb_AddUpdatePatHomePhone '$updated[patient_id_cp]', '$updated[phone1]'");
+            echo "
+            RAN $updated[first_name] $updated[last_name] $updated[birth_date] SirumWeb_AddUpdatePatHomePhone '$updated[patient_id_cp]', '$updated[phone1]'";
+          }
+          else if (strlen($updated['old_phone1']) >= 10) {
+            $sql = "UPDATE wp_usermeta SET phone = $updated[old_phone1] WHERE user_id = $updated[patient_id_wc] AND meta_key = 'phone'";
+            echo "
+            UPDATING phone1 within WC $updated[first_name] $updated[last_name] $updated[birth_date] $key $changed[$key] $sql";
+            //$mysql->run($sql);
+          }
         }
 
         if ($SirumWeb_AddUpdatePatCellPhone && $key == 'phone2' && (strlen($updated['phone2']) >= 10)) {
