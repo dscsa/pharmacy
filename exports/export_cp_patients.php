8809f91cc58ba847ab7f20614cf0eab6a0d0270b
akircher <adam@Adams-MacBook-Pro-2.local>
2020-02-02 23:34:49 -0500

    Fallback if PAT.CMT doesn't have a ___ delimiter yet

--- exports/export_cp_patients.php
+++ exports/export_cp_patients.php
@@ -5,8 +5,8 @@ function export_cp_patient_save_medications_other($mssql, $patient, $live = fals
   $sql = "
     UPDATE cppat
     SET cmt =
-      LEFT(CAST(cmt as VARCHAR(3072)), CHARINDEX(CHAR(10)+'___', cmt))+
-      '______________________________________________'+CHAR(13)+
+      SUBSTRING(cmt, 0, ISNULL(NULLIF(CHARINDEX(CHAR(10)+'___', cmt), 0), DATALENGTH(cmt)+1))+
+      CHAR(10)+'______________________________________________'+CHAR(13)+
       '$patient[medications_other]'
     WHERE pat_id = $patient[patient_id_cp]
   ";
@@ -19,14 +19,7 @@ function export_cp_patient_save_medications_other($mssql, $patient, $live = fals
 
 function export_cp_patient_save_patient_note($mssql, $patient, $live = false) {
 
-  $sql = "
-    UPDATE cppat
-    SET cmt =
-      '$patient[patient_note]'+
-      '______________________________________________'+CHAR(13)+
-      CASE WHEN DATALENGTH(cmt) > 5 THEN RIGHT(CAST(cmt as VARCHAR(3072)), DATALENGTH(cmt)-CHARINDEX('___'+CHAR(13), cmt)-5) ELSE '' END,
-    WHERE pat_id = $patient[patient_id_cp]
-  ";
+  $sql = "NOT IMPLEMENTED";
 
   echo "
   live:$live $sql";
