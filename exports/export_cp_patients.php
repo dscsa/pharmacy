231564521d038946d84dbf1325e65549cab59cad
akircher <adam@Adams-MacBook-Pro-2.local>
2020-02-05 10:55:48 -0500

    Issues with our note/meds divider so do a debugging select query

--- exports/export_cp_patients.php
+++ exports/export_cp_patients.php
@@ -3,6 +3,20 @@
 function export_cp_patient_save_medications_other($mssql, $patient, $live = false) {
 
   $medications_other = str_replace("'", "''", $patient['medications_other']);
+
+  $select = "
+    SELECT
+      LEN(cmt) as cmt_length,
+      CHARINDEX(CHAR(10), cmt) as char10_index,
+      CHARINDEX(CHAR(13), cmt) as char13_index,
+      CHARINDEX(CHAR(10)+'___', cmt) as divider_index,
+      LEN(SUBSTRING(cmt, 0, ISNULL(NULLIF(CHARINDEX(CHAR(10)+'___', cmt), 0), 9999))) as first_length,
+      SUBSTRING(cmt, 0, ISNULL(NULLIF(CHARINDEX(CHAR(10)+'___', cmt), 0), 9999)) as first,
+      cmt
+    FROM cppat
+    WHERE pat_id = $patient[patient_id_cp]
+  ";
+
   $sql = "
     UPDATE cppat
     SET cmt =
@@ -12,10 +26,12 @@ function export_cp_patient_save_medications_other($mssql, $patient, $live = fals
     WHERE pat_id = $patient[patient_id_cp]
   ";
 
-  echo "
-  live:$live $patient[first_name] $patient[last_name] $sql";
-
+  $res1 = $mssql->run("$select");
   $mssql->run("$sql");
+  $res2 = $mssql->run("$select");
+
+  echo "
+  live:$live $patient[first_name] $patient[last_name] $sql ".json_encode($res1, JSON_PRETTY_PRINT)." ".json_encode($res2, JSON_PRETTY_PRINT);
 }
 
 function export_cp_patient_save_patient_note($mssql, $patient, $live = false) {
