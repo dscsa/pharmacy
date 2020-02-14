c0e104cd161037de46adca19265c8207d88ebb04
akircher <adam@Adams-MacBook-Pro-2.local>
2020-02-13 20:03:30 -0500

    Fix patients who needed form kept showing up in orders_wc deleted feed

--- updates/update_orders_wc.php
+++ updates/update_orders_wc.php
@@ -73,16 +73,15 @@ function update_orders_wc() {
   //2) An order is in CP but not in (never added to) WC, probably because of a tech bug.
   foreach($changes['deleted'] as $deleted) {
 
+    $order = get_full_order($deleted, $mysql);
+
+    /* TODO Investigate if/why this is needed */
+    if ( ! $order) continue;
 
     if ($deleted['order_stage_wc'] == 'trash') {
 
       if ($deleted['tracking_number']) {
         log_notice("Shipped Order deleted from trash in WC. Why?", $deleted);
-
-        /* TODO Investigate if/why this is needed */
-        $order = get_full_order($deleted, $mysql);
-
-        if ( ! $order) continue;
 
         $order = helper_update_payment($order, $mysql);
 
@@ -94,11 +93,28 @@ function update_orders_wc() {
         export_gd_publish_invoice($order);
       }
 
+    } else if ( ! $order[0]['pharmacy_name']) {
+      //RXs created an order in CP but patient has not yet registered so there is no order in WC yet
+
+      $order = helper_update_payment($order, $mysql);
+
+      $reason = "update_orders_wc: RXs created an order in CP but patient has not yet registered so there is no order in WC yet";
+
+      export_wc_create_order($order,  $reason);
+
+      //TODO eventually set registration comm-calendar event then delete order but right now just remove items from order
+      //If all items are removed, order will not be imported from CP
+      $items_to_remove = [];
+      foreach($order as $item) {
+        if ($item['item_date_added'] AND $item['item_added_by'] != 'MANUAL' AND ! $item['days_dispensed'])
+          $items_to_remove[] = $item['rx_number'];
+      }
+
+      export_cp_remove_items($item['invoice_number'], $items_to_remove);
+
+      log_notice($reason, $order[0]);
+
     } else if ($deleted['order_stage_cp'] != 'Shipped' AND $deleted['order_stage_cp'] != 'Dispensed') {
-
-      $order = get_full_order($deleted, $mysql);
-
-      if ( ! $order) continue;
 
       $order = helper_update_payment($order, $mysql);
 
@@ -109,17 +125,13 @@ function update_orders_wc() {
 
       export_gd_publish_invoice($order);
 
-      //log_notice("CP Created Order that has not been saved in WC yet", $deleted);
+      //log_notice("CP Created Order that has not been saved in WC yet", $order[0]);
 
     } else {
 
       $gp_orders_wc = $mysql->run("SELECT * FROM gp_orders_wc WHERE $deleted[invoice_number]")[0];
       $gp_orders = $mysql->run("SELECT * FROM gp_orders WHERE $deleted[invoice_number]")[0];
       $wc_orders = wc_get_post_id($invoice_number);
-
-      $order = get_full_order($deleted, $mysql);
-
-      if ( ! $order) continue;
 
       $order = helper_update_payment($order, $mysql);
 
@@ -130,7 +142,7 @@ function update_orders_wc() {
 
       export_gd_publish_invoice($order);
 
-      log_notice("Readding Order that should not have been deleted. Not sure: WC Order Deleted not through trash?", [$deleted, $gp_orders_wc, $gp_orders, $wc_orders]);
+      log_notice("Readding Order that should not have been deleted. Not sure: WC Order Deleted not through trash?", [$order[0], $gp_orders_wc, $gp_orders, $wc_orders]);
     }
 
   }
