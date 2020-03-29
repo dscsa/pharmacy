ae40e0bda65a9672a8934d2ab29899987074c19e
akircher <adam@Adams-MacBook-Pro-2.local>
2020-03-29 19:43:27 -0400

    code not correctly nested within if block

--- helpers/helper_syncing.php
+++ helpers/helper_syncing.php
@@ -205,9 +205,10 @@ function set_sync_to_date($order, $target_date, $target_rxs, $mysql) {
       ";
 
       $mysql->run($sql);
+
+      $order[$i] = export_cp_set_rx_message($order[$i], RX_MESSAGE['NO ACTION SYNC TO DATE'], $mysql);
     }
 
-    $order[$i] = export_cp_set_rx_message($order[$i], RX_MESSAGE['NO ACTION SYNC TO DATE'], $mysql);
     export_v2_add_pended($order[$i], $mysql); //Days should be finalized now
   }
 
