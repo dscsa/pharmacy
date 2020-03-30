276076503e9b37e1c4254543d097d61491183def
akircher <adam@Adams-MacBook-Pro-2.local>
2020-03-30 11:59:31 -0400

    Wrong sig parsing boolean

--- helpers/helper_parse_sig.php
+++ helpers/helper_parse_sig.php
@@ -20,7 +20,7 @@ function parse_sig($sig_actual, $drug_name, $correct = null) {
   $parsed['frequency_denominators'] = frequency_denominators($parsed['durations'], $correct);
   $parsed['frequencies'] = frequencies($parsed['durations'], $correct);
 
-  if (strpos($drug_name, ' INH') !== false) {
+  if (strpos($drug_name, ' INH') === false) {
     $parsed['sig_days']    = array_sum($parsed['durations']);
     $parsed['sig_qty']     = sig_qty($parsed);
     $parsed['qty_per_day'] = qty_per_day($parsed);
