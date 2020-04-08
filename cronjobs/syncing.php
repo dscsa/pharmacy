bbc529b697d58a8e7ba9de461e1aea083e429b3c
akircher <adam@Adams-MacBook-Pro-2.local>
2020-04-08 11:39:36 -0400

    Add try catch

--- cronjobs/syncing.php
+++ cronjobs/syncing.php
@@ -33,63 +33,67 @@ $f = fopen('readme.md', 'w') or log_error('Webform Cron Job Cannot Create Lock F
 if ( ! flock($f, LOCK_EX | LOCK_NB))
   return log_error('Skipping Webform Cron Job Because Previous One Is Still Running');
 
-timer("", $time);
-$email = '';
+  try {
+  timer("", $time);
+  $email = '';
 
-//Imports
-import_wc_orders();
-$email .= timer("import_wc_orders", $time);
+  //Imports
+  import_wc_orders();
+  $email .= timer("import_wc_orders", $time);
 
-import_cp_orders(); //Put this after wc_orders so that we never have an wc_order without a matching cp_order
-$email .= timer("import_cp_orders", $time);
+  import_cp_orders(); //Put this after wc_orders so that we never have an wc_order without a matching cp_order
+  $email .= timer("import_cp_orders", $time);
 
-import_cp_order_items(); //Put this after orders so that we never have an order without a matching order_item
-$email .= timer("import_cp_order_items", $time);
+  import_cp_order_items(); //Put this after orders so that we never have an order without a matching order_item
+  $email .= timer("import_cp_order_items", $time);
 
-import_cp_patients(); //Put this after orders so that we never have an order without a matching patient
-$email .= timer("import_cp_patients", $time);
+  import_cp_patients(); //Put this after orders so that we never have an order without a matching patient
+  $email .= timer("import_cp_patients", $time);
 
-import_wc_patients(); //Put this after cp_patients so that we always import all new cp_patients first, so that out wc_patient created feed does not give false positives
-$email .= timer("import_wc_patients", $time);
+  import_wc_patients(); //Put this after cp_patients so that we always import all new cp_patients first, so that out wc_patient created feed does not give false positives
+  $email .= timer("import_wc_patients", $time);
 
-import_cp_rxs_single(); //Put this after order_items so that we never have an order item without a matching rx
-$email .= timer("import_cp_rxs_single", $time);
+  import_cp_rxs_single(); //Put this after order_items so that we never have an order item without a matching rx
+  $email .= timer("import_cp_rxs_single", $time);
 
-import_v2_stock_by_month(); //Put this after rxs so that we never have a rxs without a matching stock level
-$email .= timer("import_v2_stock_by_month", $time);
+  import_v2_stock_by_month(); //Put this after rxs so that we never have a rxs without a matching stock level
+  $email .= timer("import_v2_stock_by_month", $time);
 
-import_v2_drugs(); //Put this after rxs so that we never have a rxs without a matching drug
-$email .= timer("import_v2_drugs", $time);
+  import_v2_drugs(); //Put this after rxs so that we never have a rxs without a matching drug
+  $email .= timer("import_v2_drugs", $time);
 
-//Updates (Mirror Ordering of the above - not sure how necessary this is)
-update_drugs();
-$email .= timer("update_drugs", $time);
+  //Updates (Mirror Ordering of the above - not sure how necessary this is)
+  update_drugs();
+  $email .= timer("update_drugs", $time);
 
-update_stock_by_month();
-$email .= timer("update_stock_by_month", $time);
+  update_stock_by_month();
+  $email .= timer("update_stock_by_month", $time);
 
-update_rxs_single();
-$email .= timer("update_rxs_single", $time);
+  update_rxs_single();
+  $email .= timer("update_rxs_single", $time);
 
-update_patients_cp();
-$email .= timer("update_patients_cp", $time);
+  update_patients_cp();
+  $email .= timer("update_patients_cp", $time);
 
-update_patients_wc();
-$email .= timer("update_patients_wc", $time);
+  update_patients_wc();
+  $email .= timer("update_patients_wc", $time);
 
-update_order_items();
-$email .= timer("update_order_items", $time);
+  update_order_items();
+  $email .= timer("update_order_items", $time);
 
-update_orders_cp();
-$email .= timer("update_orders_cp", $time);
+  update_orders_cp();
+  $email .= timer("update_orders_cp", $time);
 
-update_orders_wc();
-$email .= timer("update_orders_wc", $time);
+  update_orders_wc();
+  $email .= timer("update_orders_wc", $time);
 
-watch_invoices();
-$email .= timer("watch_invoices", $time);
+  watch_invoices();
+  $email .= timer("watch_invoices", $time);
 
-if ($email) {
-  log_notice("WebForm CRON Finished", $email);
-  mail(DEBUG_EMAIL, "Log Notices", log_notices(), "From: webform@goodpill.org\r\n");
+  if ($email) {
+    log_notice("WebForm CRON Finished", $email);
+    mail(DEBUG_EMAIL, "Log Notices", log_notices(), "From: webform@goodpill.org\r\n");
+  }
+} catch (Exception $e) {
+  log_error('Webform Cron Job Fatal Error', $e);
 }
