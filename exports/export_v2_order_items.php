dd3eaeaf2b8a722f1b80862ca77b1df1d4c4610c
akircher <adam@Adams-MacBook-Pro-2.local>
2020-04-15 12:08:48 -0400

    More debugging

--- exports/export_v2_order_items.php
+++ exports/export_v2_order_items.php
@@ -156,7 +156,7 @@ function pend_pick_list($item, $vals) {
   //Pend after all forseeable errors are accounted for.
   $res = v2_fetch($pend_url, 'POST', $vals['pend']);
 
-  log_error("WebForm pend_pick_list", ['pend' => $vals['pend'], 'item' => $item, 'pend_url' => $pend_url, 'count list' => count($vals['list']), 'count pend' => count($vals['pend'])]);
+  log_error("WebForm pend_pick_list", ['res' => $res, 'pend' => $vals['pend'], 'item' => $item, 'pend_url' => $pend_url, 'count list' => count($vals['list']), 'count pend' => count($vals['pend'])]);
 }
 
 function make_pick_list($item) {
