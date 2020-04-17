74d51b95d7f31deddf3f705b6e71077cc4af0889
akircher <adam@Adams-MacBook-Pro-2.local>
2020-04-17 11:23:07 -0400

    Missing end of regex

--- helpers/helper_parse_sig.php
+++ helpers/helper_parse_sig.php
@@ -86,7 +86,7 @@ function clean_sig($sig) {
   $sig = preg_replace('/\\b([0-9]*\.[0-9]+|[1-9][0-9]*) *or *([0-9]*\.[0-9]+|[1-9][0-9]*)\\b/i', '$1', $sig, 1); //Take 1 or 2 every 3 or 4 hours. Let's convert that to Take 2 every 3 or 4 hours (no global flag).  CK approves of first substitution but not sure of the 2nd so the conservative answer is to leave it alone
   $sig = preg_replace('/\\b([0-9]*\.[0-9]+|[1-9][0-9]*) *to *([0-9]*\.[0-9]+|[1-9][0-9]*)\\b/i', '$1', $sig, 1); //Take 1 to 2 every 3 or 4 hours. Let's convert that to Take 2 every 3 or 4 hours (no global flag).  CK approves of first substitution but not sure of the 2nd so the conservative answer is to leave it alone
   $sig = preg_replace('/\\b([0-9]*\.[0-9]+|[1-9][0-9]*) *- *([0-9]*\.[0-9]+|[1-9][0-9]*)\\b/i', '$1', $sig, 1); //Take 1-2 every 3 or 4 hours. Let's convert that to Take 2 every 3 or 4 hours (no global flag).  CK approves of first substitution but not sure of the 2nd so the conservative answer is to leave it alone
-  $sig = preg_replace('/\\b(exceed (more than )?|exceeding |not to |max(imum)? (of |per day (of )?|daily dose( |: ?))/i', 'max ', $sig);
+  $sig = preg_replace('/\\b(exceed (more than )?|exceeding |not to |max(imum)? (of |per day (of )?|daily dose( |: ?))?)/i', 'max ', $sig);
 
   //Duration
   $sig = preg_replace('/\\bx ?(\d+)\\b/i', 'for $1', $sig); // X7 Days == for 7 days
