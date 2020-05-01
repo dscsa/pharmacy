40b13d8dfae16edd0c5728b30aced8d28e5648c4
akircher <adam@Adams-MacBook-Pro-2.local>
2020-05-01 12:00:57 -0400

    rx_number needs quotes

--- cronjobs/rxmessage.php
+++ cronjobs/rxmessage.php
@@ -5,6 +5,7 @@
 ini_set('include_path', '/goodpill/webform');
 date_default_timezone_set('America/New_York');
 
+require_once 'helpers/helper_log.php';
 require_once 'dbs/mysql_wc.php';
 require_once 'dbs/mssql_cp.php';
 
@@ -28,27 +29,27 @@ $count = count($rxs);
 
 foreach ($rxs as $i => $rx) {
   if ($rx['stock_level_initial'] == 'HIGH SUPPLY') {
-    $mssql->run("UPDATE cprx SET priority_cn = 218 WHERE script_no = $rx[rx_number]");
+    $mssql->run("UPDATE cprx SET priority_cn = 218 WHERE script_no = '$rx[rx_number]'");
     echo "$i of $count, $rx[rx_message_key] $rx[stock_level_initial]";
   }
 
   if ($rx['stock_level_initial'] == 'LOW SUPPLY') {
-    $mssql->run("UPDATE cprx SET priority_cn = 219 WHERE script_no = $rx[rx_number]");
+    $mssql->run("UPDATE cprx SET priority_cn = 219 WHERE script_no = '$rx[rx_number]'");
     echo "$i of $count, $rx[rx_message_key] $rx[stock_level_initial]";
   }
 
   if ($rx['stock_level_initial'] == 'REFILL ONLY') {
-    $mssql->run("UPDATE cprx SET priority_cn = 220 WHERE script_no = $rx[rx_number]");
+    $mssql->run("UPDATE cprx SET priority_cn = 220 WHERE script_no = '$rx[rx_number]'");
     echo "$i of $count, $rx[rx_message_key] $rx[stock_level_initial]";
   }
 
   if ($rx['stock_level_initial'] == 'OUT OF STOCK') {
-    $mssql->run("UPDATE cprx SET priority_cn = 221 WHERE script_no = $rx[rx_number]");
+    $mssql->run("UPDATE cprx SET priority_cn = 221 WHERE script_no = '$rx[rx_number]'");
     echo "$i of $count, $rx[rx_message_key] $rx[stock_level_initial]";
   }
 
   if ($rx['stock_level_initial'] == 'ONE TIME') {
-    $mssql->run("UPDATE cprx SET priority_cn = 222 WHERE script_no = $rx[rx_number]");
+    $mssql->run("UPDATE cprx SET priority_cn = 222 WHERE script_no = '$rx[rx_number]'");
     echo "$i of $count, $rx[rx_message_key] $rx[stock_level_initial]";
   }
 }
