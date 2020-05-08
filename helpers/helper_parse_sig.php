463721a9c9afe6591314f30232e0b94dfa6f8e56
akircher <adam@Adams-MacBook-Pro-2.local>
2020-05-08 12:45:50 -0400

    Fix sig Take 1 tablet by mouth  three times per day for 14 days then once daily for 16 days

--- helpers/helper_parse_sig.php
+++ helpers/helper_parse_sig.php
@@ -250,7 +250,7 @@ function qtys_per_time($durations, $drug_name, $correct) {
 
   foreach ($durations as $sig_part => $duration) {
     //"Use daily with lantus"  won't match the RegEx below
-    $count = preg_match_all('/([0-9]*\.[0-9]+|[1-9][0-9]*) ?(ml|tab|cap|pill|softgel|patch|injection|each)|(^|use +|max +|maximum +|total +of +|take +|inhale +|chew +|inject +|oral +)([0-9]*\.[0-9]+|[1-9][0-9]*)(?!\d| *\.| *mg| *time| *min| *hour| *day| *week| *month)/i', $sig_part, $match);
+    $count = preg_match_all('/([0-9]*\.[0-9]+|[1-9][0-9]*) ?(ml|tab|cap|pill|softgel|patch|injection|each|time)|(^|use +|max +|maximum +|total +of +|take +|inhale +|chew +|inject +|oral +)([0-9]*\.[0-9]+|[1-9][0-9]*)(?!\d| *\.| *mg| *time| *min| *hour| *day| *week| *month)/i', $sig_part, $match);
 
     //print_r(['duration' => $duration, 'match' => $match, 'count' => $count]);
 
