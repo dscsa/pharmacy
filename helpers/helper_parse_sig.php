77c77b17ab9cd15cf069b3c22793f299bba0d127
akircher <adam@Adams-MacBook-Pro-2.local>
2020-05-18 16:23:35 -0400

    More sig fixing for Nitroglycerin

--- helpers/helper_parse_sig.php
+++ helpers/helper_parse_sig.php
@@ -66,6 +66,8 @@ function clean_sig($sig) {
   $sig = preg_replace('/\\\/', '', $sig);   //get rid of backslashes and single quotes (latter for sql savings errors)
   $sig = preg_replace('/\\band\\b/i', '&', $sig);   // & is easier tp search in regex than "and"
   $sig = preg_replace('/\\bDr\./i', '', $sig);   // The period in Dr. will mess up our split for durations
+  $sig = preg_replace('/\\bthen call 911/i', '', $sig); //"then call 911" is not a duration
+
 
   $sig = preg_replace('/ +(mc?g)\\b| +(ml)\\b/i', '$1', $sig);   //get rid of extra spaces
   $sig = preg_replace('/[\w ]*replaces[\w ]*/i', '$1', $sig); //Take 2 tablets (250 mcg total) by mouth daily. This medication REPLACES Levothyroxine 112 mcg",
@@ -209,7 +211,7 @@ function clean_sig($sig) {
 
   //If MAX then use max and ignore preceeding sig e.g TAKE 1 TABLET BY MOUTH AS NEEDED FOR MIGRAINE, MAY REPEAT IN 2 HRS IF NEEDED, MAX 2TABS\/24 HRS
   //Put this after inject because max 350 units may become 1 injection
-  $sig = preg_replace('/.*(exceed (more than )?|every \d minutes? for |exceeding |totaling |up ?to |total of |not to |max(imum)? (of |per day (of )?|per day dose( |: ?)|daily dose( |: ?))?)([0-9]*\.[0-9]+|[1-9][0-9]*)/i', 'Max $8', $sig); //Get rid of "max" qtys in sig because they are redundant and could accidentally be added in
+  $sig = preg_replace('/.*(exceed (more than )?|every \d minutes? for |exceeding |totaling |up ?to |total of |not to |no more than |max(imum)? (of |per day (of )?|per day dose( |: ?)|daily dose( |: ?))?)([0-9]*\.[0-9]+|[1-9][0-9]*)/i', 'Max $8', $sig); //Get rid of "max" qtys in sig because they are redundant and could accidentally be added in
 
   //Cleanup
   $sig = preg_replace('/  +/i', ' ', $sig); //Remove double spaces for aesthetics
@@ -378,7 +380,7 @@ function frequencies($durations, $correct) {
       $freq = $as_needed ? '2/24' : '1/24'; // One 24th of a day
 
     else if (preg_match('/ minute(?!s? +before|s? +after|s? +prior to)/i', $sig_part)) //put this last so less likely to match thinks like "2 hours before (meals|bedtime) every day"
-      $freq = $as_needed ? '90/25*3*5' : '1/24/60'; //Most common is Nitroglycerin: 1 tab every 5 minutes for chest pain.  Since comes in boxes of 25 each, Cindy likes dispensing 75 for 90 days
+      $freq = $as_needed ? '90/25/3/5' : '1/24/60'; //Most common is Nitroglycerin: 1 tab every 5 minutes for chest pain.  Since comes in boxes of 25 each, Cindy likes dispensing 75 for 90 days
 
     else if (preg_match('/at (1st|first) sign|when|at onset|(if|for) chest pain|(if|for) CP/i', $sig_part)) //CP is shorthand for Chest Pain
       $freq = '90/25'; //Most common is Nitroglycerin: max 3 doses for chest pain.  Since comes in boxes of 25 each, Cindy likes dispensing 75 for 90 days (90*3/3.6 = 75)
