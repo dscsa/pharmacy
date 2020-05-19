bbc73ee58fbb552c20eab03fc2c7da1f1e7ce5cc
akircher <adam@Adams-MacBook-Pro-2.local>
2020-05-19 15:48:36 -0400

    Limit duration of PEG to 1 day

--- helpers/helper_parse_sig.php
+++ helpers/helper_parse_sig.php
@@ -190,7 +190,7 @@ function clean_sig($sig) {
   $sig = preg_replace('/\\b(q1.*?h|every hour)\\b/i', 'every 1 hours', $sig);
 
   //Alternate units of measure
-  $sig = preg_replace('/\\b4,?000ml\\b/i', '1', $sig); //We count Polyethylene Gylcol (PEG) as 1 unit not 4000ml.  TODO Maybe replace this rule with a more generalized rule?
+  $sig = preg_replace('/\\b4,?000ml\\b/i', '1 for 1 day', $sig); //We count Polyethylene Gylcol (PEG) as 1 unit not 4000ml.  TODO Maybe replace this rule with a more generalized rule?
   $sig = preg_replace('/\\b1 vial\\b/i', '3ml', $sig); // vials for inhalation are 2.5 or 3ml, so use 3ml to be conservative
   $sig = preg_replace('/\\b2 vials?\\b/i', '6ml', $sig); // vials for inhalation are 2.5 or 3ml, so use 3ml to be conservative
   $sig = preg_replace('/\\b3 vials?\\b/i', '9ml', $sig); // vials for inhalation are 2.5 or 3ml, so use 3ml to be conservative
