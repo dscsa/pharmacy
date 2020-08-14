82a8863fb15657951edb6901dd15afb8078ecbb6
akircher <adam@Adams-MacBook-Pro-2.local>
2020-08-14 12:46:16 -0400

    Weird encoding issue where ' is showing up as â€™

--- helpers/helper_imports.php
+++ helpers/helper_imports.php
@@ -3,7 +3,8 @@
 function escape_db_values($raw) {
   //@mysql_escape_string(stripslashes(trim($clean))) is removed from php 7 and mysqli version requires db connection
   //stripslashes only remove one slash so \\1 become \1 and we want to remove all
-  return preg_replace("/(^|[^'])'([^']|$)/i", "$1''$2", str_replace('\\', '', trim($raw)));
+
+  return preg_replace("/(^|[^'])'([^']|$)/i", "$1''$2", str_replace(['\\', 'â€™'], ['',"'"], trim($raw)));
 }
 
 // Convert empty string to null or CP's <Not Specified> to NULL
