a6c6747da81e70bda8dc1959856cd0607df0efa2
akircher <adam@Adams-MacBook-Pro-2.local>
2020-08-21 10:19:24 -0400

    Workaround to stop spamming joseph

--- helpers/helper_days_dispensed.php
+++ helpers/helper_days_dispensed.php
@@ -211,7 +211,8 @@ function get_days_default($item, $order) {
 
       $event_title = "$item[invoice_number] Refill Out Of Stock: $salesforce[contact] Created:".date('Y-m-d H:i:s');
 
-      create_event($event_title, [$salesforce]);
+      if (stripos($item['first_name'], 'TEST') == FALSE AND stripos($item['last_name'], 'TEST') == FALSE)
+        create_event($event_title, [$salesforce]);
     }
     else
       log_notice("WARN USERS IF DRUG IS LOW QTY", get_defined_vars());
