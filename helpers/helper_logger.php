3a7f62f4bae73e9f8efb06177e9ba27062304f81
Ben Brown <ben@sirum.org>
2020-10-13 11:30:26 -0400

    Changing the path to the proper path on the server.

--- helpers/helper_logger.php
+++ helpers/helper_logger.php
@@ -1,10 +1,10 @@
 <?php
 
 require_once 'vendor/autoload.php';
+
 use Google\Cloud\Logging\LoggingClient;
 
-putenv('GOOGLE_APPLICATION_CREDENTIALS=unified-logging.json');
-# putenv('GOOGLE_APPLICATION_CREDENTIALS=/etc/google/unified-logging.json');
+putenv('GOOGLE_APPLICATION_CREDENTIALS=/etc/google/unified-logging.json');
 
 $logging = new LoggingClient([
     'projectId' => 'unified-logging-292316'
