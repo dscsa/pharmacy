5bdef63e1d35dcc763aec6f702db9e4e6a273575
akircher <adam@Adams-MacBook-Pro-2.local>
2020-11-24 22:02:52 -0500

    Seems like large log files are silently failing

--- imports/import_wc_patients.php
+++ imports/import_wc_patients.php
@@ -76,7 +76,7 @@ function import_wc_patients() {
      wp_users.ID
   ");
 
-  log_error("import_wc_patients start: ", ['vals' => array_slice($patients[0], 0, 100)]);
+  log_error("import_wc_patients start: ", ['vals' => array_slice($patients[0], 0, 100, true)]);
 
   if ( ! count($patients[0])) return log_error('No Wc Patients to Import', get_defined_vars());
 
@@ -113,9 +113,9 @@ function import_wc_patients() {
     }
   );
 
-  log_error("import_wc_patients cleaned: ", ['keys' => $keys, 'vals' => array_slice($patients[0], 0, 100)]);
+  log_error("import_wc_patients cleaned: ", ['keys' => array_slice($keys, 0, 100, true), 'vals' => array_slice($patients[0], 0, 100, true)]);
 
   $mysql->replace_table("gp_patients_wc", $keys, $patients[0]);
 
-  log_error("import_wc_patients finish: ", ['keys' => $keys, 'vals' => array_slice($patients[0], 0, 100)]);
+  log_error("import_wc_patients finish: ", ['keys' => array_slice($keys, 0, 100, true), 'vals' => array_slice($patients[0], 0, 100, true)]);
 }
