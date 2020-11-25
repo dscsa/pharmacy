05b29399c071ae7254bd1452066d121ae863d828
akircher <adam@Adams-MacBook-Pro-2.local>
2020-11-24 21:49:03 -0500

    Logs still not showing up, are the values too big?

--- imports/import_wc_patients.php
+++ imports/import_wc_patients.php
@@ -76,7 +76,7 @@ function import_wc_patients() {
      wp_users.ID
   ");
 
-  log_error("import_wc_patients start: ", ['vals' => $patients[0]]);
+  log_error("import_wc_patients start: ", ['vals' => array_slice($patients[0], 0, 100)]);
 
   if ( ! count($patients[0])) return log_error('No Wc Patients to Import', get_defined_vars());
 
@@ -113,9 +113,9 @@ function import_wc_patients() {
     }
   );
 
-  log_error("import_wc_patients cleaned: ", ['keys' => $keys, 'vals' => $patients[0]]);
+  log_error("import_wc_patients cleaned: ", ['keys' => $keys, 'vals' => array_slice($patients[0], 0, 100)]);
 
   $mysql->replace_table("gp_patients_wc", $keys, $patients[0]);
 
-  log_error("import_wc_patients finish: ", ['keys' => $keys, 'vals' => $patients[0]]);
+  log_error("import_wc_patients finish: ", ['keys' => $keys, 'vals' => array_slice($patients[0], 0, 100)]);
 }
