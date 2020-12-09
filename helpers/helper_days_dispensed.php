8f3b8b2ee4f2f1b5295b74dcc486e251f788f67c
akircher <adam@Adams-MacBook-Pro-2.local>
2020-12-09 12:44:59 -0500

    Not Due needs to always be determine from date_default, otherwise it could be manipulated by the user

--- helpers/helper_days_dispensed.php
+++ helpers/helper_days_dispensed.php
@@ -141,7 +141,7 @@ function get_days_default($item, $patient_or_order) {
     return [$days_default, RX_MESSAGE['NO ACTION PATIENT REQUESTED']];
   }
 
-  if ($is_order AND (strtotime($item['refill_date_next']) - strtotime($item['order_date_added'])) > DAYS_EARLY*24*60*60 AND ! $added_manually) {
+  if ($is_order AND (strtotime($item['refill_date_default']) - strtotime($item['order_date_added'])) > DAYS_EARLY*24*60*60 AND ! $added_manually) {
 
     //DON'T STRICTLY NEED THIS TEST BUT IT GIVES A MORE SPECIFIC ERROR SO IT MIGHT BE HELPFUL
     if ((strtotime($item['order_date_added']) - strtotime($item['refill_date_last'])) < DAYS_EARLY*24*60*60 AND ! $added_manually) {
