fd47dd3605af264ff318ab1257e4368b8f0d9d9a
akircher <adam@Adams-MacBook-Pro-2.local>
2020-12-16 20:26:34 -0500

    Don't group order items, we want 1-to-1 correlation to CP so we can dedup as necessary

--- imports/import_cp_order_items.php
+++ imports/import_cp_order_items.php
@@ -40,7 +40,7 @@ function import_cp_order_items() {
   	JOIN cprx ON cprx.rx_id = csomline.rx_id
     LEFT JOIN cprx_disp disp ON csomline.rxdisp_id > 0 AND disp.rxdisp_id = csomline.rxdisp_id -- Rx might not yet be dispensed
     WHERE dispense_date IS NULL OR dispense_date > @today - 7  --Undispensed and dispensed within the week only to cut down volume. i think this still enables qty/days_dispensed_actual to be set properly
-    GROUP BY csomline.order_id, (CASE WHEN gcn_seqno > 0 THEN gcn_seqno ELSE script_no END) --This is because of Orders like 8660 where we had 4 duplicate Citalopram 40mg.  Two that were from Refills, One Denied Surescript Request, and One new Surescript.  We are only going to send one GCN so don't list it multiple times
+    GROUP BY line_id -- csomline.order_id, (CASE WHEN gcn_seqno > 0 THEN gcn_seqno ELSE script_no END) --This is because of Orders like 8660 where we had 4 duplicate Citalopram 40mg.  Two that were from Refills, One Denied Surescript Request, and One new Surescript.  We are only going to send one GCN so don't list it multiple times
   ");
 
   if ( ! count($items[0])) return log_error('No Cp Order Items to Import', get_defined_vars());
