674c9fb1e401fd8ad16b53d615d5c14cdad6ce33
akircher <adam@Adams-MacBook-Pro-2.local>
2020-12-21 10:04:59 -0500

    Enable load_full_item() to get shipped orders if invoice_number is specified

--- helpers/helper_full_item.php
+++ helpers/helper_full_item.php
@@ -9,29 +9,53 @@ function load_full_item($item, $mysql, $overwrite_rx_messages = false) {
     return [];
   }
 
-  $sql = "
-    SELECT
-      *,
-      gp_rxs_grouped.*,
-      gp_orders.invoice_number,
-      gp_order_items.invoice_number as dontuse_item_invoice,
-      gp_orders.invoice_number as dontuse_order_invoice
-    FROM
-      gp_rxs_single
-    JOIN gp_patients ON
-      gp_rxs_single.patient_id_cp = gp_patients.patient_id_cp
-    JOIN gp_rxs_grouped ON
-      rx_numbers LIKE CONCAT('%,', gp_rxs_single.rx_number, ',%')
-    LEFT JOIN gp_stock_live ON -- might not have a match if no GSN match
-      gp_rxs_grouped.drug_generic = gp_stock_live.drug_generic
-    LEFT JOIN gp_order_items ON -- choice to show any order_item from this rx_group and not just if this specific rx matches
-      gp_order_items.rx_dispensed_id IS NULL AND
-      rx_numbers LIKE CONCAT('%,', gp_order_items.rx_number, ',%')
-    LEFT JOIN gp_orders ON -- ORDER MAY HAVE NOT BEEN ADDED YET
-      gp_orders.invoice_number = gp_order_items.invoice_number
-    WHERE
-      gp_rxs_single.rx_number = $item[rx_number]
-  ";
+  if ($item['invoice_number']) //If changeing days_dispensed_actual NULL >>> 90, then this will be true and order will be shipped
+    $sql = "
+      SELECT
+        *,
+        gp_rxs_grouped.*,
+        gp_orders.invoice_number,
+        gp_order_items.invoice_number as dontuse_item_invoice,
+        gp_orders.invoice_number as dontuse_order_invoice
+      FROM
+        gp_rxs_single
+      JOIN gp_patients ON
+        gp_rxs_single.patient_id_cp = gp_patients.patient_id_cp
+      JOIN gp_rxs_grouped ON
+        rx_numbers LIKE CONCAT('%,', gp_rxs_single.rx_number, ',%')
+      LEFT JOIN gp_stock_live ON -- might not have a match if no GSN match
+        gp_rxs_grouped.drug_generic = gp_stock_live.drug_generic
+      LEFT JOIN gp_order_items ON -- choice to show any order_item from this rx_group and not just if this specific rx matches
+        gp_order_items.invoice_number = $item[invoice_number] AND
+        rx_numbers LIKE CONCAT('%,', gp_order_items.rx_number, ',%')
+      LEFT JOIN gp_orders ON -- ORDER MAY HAVE NOT BEEN ADDED YET
+        gp_orders.invoice_number = gp_order_items.invoice_number
+      WHERE
+        gp_rxs_single.rx_number = $item[rx_number]
+    ";
+  else
+    $sql = "
+      SELECT
+        *,
+        gp_rxs_grouped.*,
+        gp_orders.invoice_number,
+        gp_order_items.invoice_number as dontuse_item_invoice,
+        gp_orders.invoice_number as dontuse_order_invoice
+      FROM
+        gp_rxs_single
+      JOIN gp_patients ON
+        gp_rxs_single.patient_id_cp = gp_patients.patient_id_cp
+      JOIN gp_rxs_grouped ON
+        rx_numbers LIKE CONCAT('%,', gp_rxs_single.rx_number, ',%')
+      LEFT JOIN gp_stock_live ON -- might not have a match if no GSN match
+        gp_rxs_grouped.drug_generic = gp_stock_live.drug_generic
+      LEFT JOIN gp_order_items ON -- choice to show any order_item from this rx_group and not just if this specific rx matches
+        gp_order_items.rx_dispensed_id IS NULL AND
+        rx_numbers LIKE CONCAT('%,', gp_order_items.rx_number, ',%')
+      LEFT JOIN gp_orders ON -- ORDER MAY HAVE NOT BEEN ADDED YET
+        gp_orders.invoice_number = gp_order_items.invoice_number
+      WHERE
+        gp_rxs_single.rx_number = $item[rx_number]
 
   $query = $mysql->run($sql);
 
