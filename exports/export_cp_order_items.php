7db01686dab8d337f54de4222eae6caff260f65b
akircher <adam@Adams-MacBook-Pro-2.local>
2020-12-25 14:43:34 -0500

    Differentiate logging for item_added but no order message

--- exports/export_cp_order_items.php
+++ exports/export_cp_order_items.php
@@ -114,16 +114,25 @@ function export_cp_add_items($invoice_number, $items) {
 
     $current_order = $mysql->run($sql)[0];
 
-    SirumLog::alert("Item needs to be added but NO Order? Confirm this is always an rx-created2/updated or deleted order-item (i understand former but not latter). Find current order if one exists.  Maybe even create a new order if one doesn't exist?"
-    ." invoice_number:".$items[0]['invoice_number']
-    ." item_invoice:".$items[0]['dontuse_item_invoice']
-    ." order_invoice:".$items[0]['dontuse_order_invoice'], [
+    $invoice_number = @$current_order[0]['invoice_number'];
+
+    $log = [
+      "subject" => "Item needs to be added",
+      "msg" => "Confirm this is always an rx-created2/updated or deleted order-item (i understand former but not latter). Find current order if one exists.  Maybe even create a new order if one doesn't exist?",
+      "invoice_number" => $items[0]['invoice_number'],
+      "item_invoice" => $items[0]['dontuse_item_invoice'],
+      "order_invoice" => $items[0]['dontuse_order_invoice'],
       'sql'   => $sql,
       'items' => $items,
       'current_order' => $current_order
-    ]);
+    ];
 
-    $invoice_number = $current_order[0]['invoice_number'];
+    if ( ! $invoice_number) {
+        SirumLog::alert("$log[subject] but no order, not even a current one", $log);
+        return;
+    }
+
+    SirumLog::warn("$log[subject] but using a different, current one", $log);
   }
 
   $sql = "SirumWeb_AddItemsToOrder '$invoice_number', '$rx_numbers'";
