2a2b5aa4bfcad3f458429c5eb4889959f5ddbec4
akircher <adam@Adams-MacBook-Pro-2.local>
2021-01-08 23:29:50 -0500

    Import didn't work, its because the functions were accidentally nested within update_drugs()

--- updates/update_drugs.php
+++ updates/update_drugs.php
@@ -103,91 +103,91 @@ function update_drugs($changes) {
 
     $mysql->run($sql);
   }
+}
 
-  function update_mismatched_rxs_and_items($mysql, $partial) {
+function update_mismatched_rxs_and_items($mysql, $partial) {
 
-    $drug_gsns = @$partial['old_drug_gsns'] ?: $partial['drug_gsns']; //update_drugs::created doesn't have old_drug_gsns
+  $drug_gsns = @$partial['old_drug_gsns'] ?: $partial['drug_gsns']; //update_drugs::created doesn't have old_drug_gsns
 
-    $sql = "
-      SELECT *
-      FROM gp_rxs_single
-      LEFT JOIN gp_order_items ON
-        gp_rxs_single.rx_number = gp_order_items.rx_number
-      WHERE
-        drug_generic != '{$partial['drug_generic']}' -- gsn was moved not just added
-        AND rx_gsn IN ($drug_gsns)
-        AND rx_dispensed_id IS NULL
-    ";
+  $sql = "
+    SELECT *
+    FROM gp_rxs_single
+    LEFT JOIN gp_order_items ON
+      gp_rxs_single.rx_number = gp_order_items.rx_number
+    WHERE
+      drug_generic != '{$partial['drug_generic']}' -- gsn was moved not just added
+      AND rx_gsn IN ($drug_gsns)
+      AND rx_dispensed_id IS NULL
+  ";
 
-    $rxs = $mysql->run($sql)[0];
+  $rxs = $mysql->run($sql)[0];
 
-    if ( ! $rxs)
-      return log_warning("update_mismatched_rxs_and_items_by_drug_gsns: no rxs_single or order_items to update", [
-        'partial' => $partial,
-        'sql' => $sql,
-        'rxs' => $rxs
-      ]);
-
-    //NOTE Since we are here, GSNs moved in such a way that order_items might have:
-    //pended for the wrong drug,
-    //have the wrong price,
-    //have the wrong initial stock level,
-    //have the wrong days (due to the above), etc
-    log_alert("update_mismatched_rxs_and_items_by_drug_gsns: updating rxs_single(s) and undispensed order_item(s)", [
+  if ( ! $rxs)
+    return log_warning("update_mismatched_rxs_and_items_by_drug_gsns: no rxs_single or order_items to update", [
       'partial' => $partial,
       'sql' => $sql,
       'rxs' => $rxs
     ]);
 
-    foreach($rxs as $rx) {
-      update_rx_single_drug($mysql, $rx['rx_number']);
-      update_order_item_drug($mysql, $rx['rx_number']);
-    }
+  //NOTE Since we are here, GSNs moved in such a way that order_items might have:
+  //pended for the wrong drug,
+  //have the wrong price,
+  //have the wrong initial stock level,
+  //have the wrong days (due to the above), etc
+  log_alert("update_mismatched_rxs_and_items_by_drug_gsns: updating rxs_single(s) and undispensed order_item(s)", [
+    'partial' => $partial,
+    'sql' => $sql,
+    'rxs' => $rxs
+  ]);
+
+  foreach($rxs as $rx) {
+    update_rx_single_drug($mysql, $rx['rx_number']);
+    update_order_item_drug($mysql, $rx['rx_number']);
   }
+}
 
-  function update_rx_single_drug($mysql, $rx_number) {
+function update_rx_single_drug($mysql, $rx_number) {
 
-    $sql_rxs_single = "
-      UPDATE gp_rxs_single
-      JOIN gp_drugs ON
-        drug_gsns LIKE CONCAT('%,', rx_gsn, ',%')
-      SET
-        gp_rxs_single.drug_generic = gp_drugs.drug_generic,
-        gp_rxs_single.drug_brand   = gp_drugs.drug_brand,
-        gp_rxs_single.drugs_gsns   = gp_drugs.drugs_gsns
-      WHERE
-        gp_order_items.rx_number = '$rx_number'
-    ";
+  $sql_rxs_single = "
+    UPDATE gp_rxs_single
+    JOIN gp_drugs ON
+      drug_gsns LIKE CONCAT('%,', rx_gsn, ',%')
+    SET
+      gp_rxs_single.drug_generic = gp_drugs.drug_generic,
+      gp_rxs_single.drug_brand   = gp_drugs.drug_brand,
+      gp_rxs_single.drugs_gsns   = gp_drugs.drugs_gsns
+    WHERE
+      gp_order_items.rx_number = '$rx_number'
+  ";
 
-    log_warning("update_rx_single_drug: updated gp_rx_single", [
-      'sql_rxs_single'  => $sql_rxs_single,
-      'rx_number'       => $rx_number
-    ]);
+  log_warning("update_rx_single_drug: updated gp_rx_single", [
+    'sql_rxs_single'  => $sql_rxs_single,
+    'rx_number'       => $rx_number
+  ]);
 
-    $mysql->run($sql_rxs_single);
-  }
+  $mysql->run($sql_rxs_single);
+}
 
-  function update_order_item_drug($mysql, $rx_number) {
+function update_order_item_drug($mysql, $rx_number) {
 
-    //Hacky but we want to FORCE $needs_repending = true in helper_full_fields so setting days to something unlikely to ever be correct
-    //Next load_full_patient/order/item should overwrite rx_messages, days_dispensed_default, and price_dispensed_default.  Should unpend and repend for the "new" drug
-    $sql_order_items = "
-      UPDATE gp_order_items
-      SET
-        days_dispensed_default = 999
-      WHERE
-        gp_order_items.rx_number  = '$rx_number'
-        AND rx_dispensed_id IS NULL
-    ";
+  //Hacky but we want to FORCE $needs_repending = true in helper_full_fields so setting days to something unlikely to ever be correct
+  //Next load_full_patient/order/item should overwrite rx_messages, days_dispensed_default, and price_dispensed_default.  Should unpend and repend for the "new" drug
+  $sql_order_items = "
+    UPDATE gp_order_items
+    SET
+      days_dispensed_default = 999
+    WHERE
+      gp_order_items.rx_number  = '$rx_number'
+      AND rx_dispensed_id IS NULL
+  ";
 
-    log_alert("update_order_item_drug: updated gp_order_item", [
-      'sql_order_items' => $sql_order_items,
-      'rx_number'       => $rx_number
-    ]);
+  log_alert("update_order_item_drug: updated gp_order_item", [
+    'sql_order_items' => $sql_order_items,
+    'rx_number'       => $rx_number
+  ]);
 
-    $mysql->run($sql_order_items);
+  $mysql->run($sql_order_items);
 
-    //overwrite == true should force rx_messages, days_dispensed_default, and price_dispensed_default to all be recalculated
-    $item = load_full_item(['rx_number' => $rx_number], $mysql, true);
-  }
+  //overwrite == true should force rx_messages, days_dispensed_default, and price_dispensed_default to all be recalculated
+  $item = load_full_item(['rx_number' => $rx_number], $mysql, true);
 }
