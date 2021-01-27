f4822162956595e91b36488051a962b41b4de178
Ben Brown <ben@sirum.org>
2021-01-27 10:42:25 -0500

    Making sure the salesforce is there.

---
+++ classes/Sirum/Notifications/Salesforce.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sirum\Notifications;
+
+use Sirum\GPModel;
+
+/**
+ * Class for a RX Transfer notifcation
+ */
+class Salesforce extends Notification
+{
+    protected $type = 'salesforce';
+}
