fe3eed1807710a0e23405458b7ff3469d2cc62f9
Ben Brown <ben@sirum.org>
2021-02-23 11:28:14 -0500

    Adding a flag to not restart services.

--- configs/pa_update.sh
+++ configs/pa_update.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/bash
 
 CODE_DIR=/goodpill/webform
+RESTART_SERVICES=1
+
+while getopts ":s" opt; do
+  case ${opt} in
+    s ) RESTART_SERVICES=0
+      ;;
+    t ) # process option t
+      ;;
+    \? ) echo "Usage: cmd [-s]"
+      ;;
+  esac
+done
 
 # Get the latest code
 cd $CODE_DIR
@@ -10,6 +22,8 @@ sudo git pull
 sudo cp $CODE_DIR/configs/pharmacy.crons /etc/cron.d/pharmacy.crons
 sudo cp $CODE_DIR/configs/supervisor.conf /etc/supervisord.conf
 
-#Restart the services
-sudo service crond restart
-sudo service supervisord restart
+if [ $RESTART_SERVICES -eq 1 ]; then
+    #Restart the services
+    sudo service crond restart
+    sudo service supervisord restart
+fi
