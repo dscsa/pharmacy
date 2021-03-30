68c7dd1bfa8645b243f40a7e3e06d52c01f05be7
Ben Brown <ben@sirum.org>
2021-03-30 10:34:01 -0400

    Adding a shim to get the capsule setup for a user.

---
+++ helpers/helper_laravel.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+if (!isset($lv_model_capsule)
+    || !($lv_model_capsule instanceof Illuminate\Database\Capsule\Manager)) {
+    $lv_model_capsule = new Capsule;
+
+    $lv_model_capsule->addConnection([
+        'driver'    => 'mysql',
+        'host'      => MYSQL_WC_IP,
+        'database'  => 'goodpill',
+        'username'  => MYSQL_WC_USER,
+        'password'  => MYSQL_WC_PWD
+    ]);
+
+    // Make this Capsule instance available globally via static methods... (optional)
+    $lv_model_capsule->setAsGlobal();
+
+    // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+    $lv_model_capsule->bootEloquent();
+}
