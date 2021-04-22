b53a726b81774d8e53e4945b7b320079bf001671
Ben Brown <ben@sirum.org>
2021-04-22 17:27:17 -0400

    Adding a search and delete class update the calendar helper to queue search and delete for cancel events

---
+++ classes/GoodPill/AWS/SQS/GoogleAppRequest/Calendar/SearchAndDelete.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace  GoodPill\AWS\SQS\GoogleAppRequest\Calendar;
+
+use GoodPill\AWS\SQS\GoogleAppRequest\HelperRequest;
+
+/**
+ * Base level class for all Google Doc requests
+ */
+class SearchAndDelete extends HelperRequest
+{
+    protected $properties = [
+        'cal_id',
+        'word_search',
+        'hours',
+        'regex_search'
+    ];
+
+    protected $required = [
+        'cal_id',
+        'word_search',
+    ];
+
+    /**
+     * Set the method to the default value for a delete request
+     * @param string $requests  (Optional) The initial data
+     */
+    public function __construct($request = null)
+    {
+        $this->method = 'v2/searchAndDeleteEvents';
+        parent::__construct($request);
+    }
+}
