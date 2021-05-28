5e7c340d5ad8ded262c2a7e513725fc9e889a1ba
Ben Brown <ben@sirum.org>
2021-05-28 11:28:22 -0400

    The SigParser namespace and filepath were wrong

--- updates/update_rxs_single.php
+++ updates/update_rxs_single.php
@@ -12,7 +12,7 @@ use GoodPill\Logging\CliLog;
 
 use GoodPill\Utilities\Timer;
 use GoodPill\Models\GpRxsSingle;
-use GoodPill\SigParser\SigParser;
+use GoodPill\utilities\SigParser;
 
 function update_rxs_single($changes)
 {
