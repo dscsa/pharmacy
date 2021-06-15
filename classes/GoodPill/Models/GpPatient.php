254e75b737c474af81eaca87648681f344ee2e7a
Ben Brown <ben@sirum.org>
2021-06-15 11:56:17 -0400

    The try wasn't stopping the exception from bubbling, so instead I switched to an if.

--- classes/GoodPill/Models/GpPatient.php
+++ classes/GoodPill/Models/GpPatient.php
@@ -355,17 +355,16 @@ class GpPatient extends Model
      */
     public function updateWpMeta(string $key, $value) : bool
     {
-        try {
-            $meta = $this->wcUser
-                         ->meta()
-                         ->firstOrNew(['meta_key' => $key]);
 
+        $wcuser = $this->wcUser;
+
+        if ($wcuser) {
+            $meta = $wcuser->meta()->firstOrNew(['meta_key' => $key]);
             $meta->meta_value = $value;
-
             return $meta->save();
-        } catch (\Exception $e) {
-            return false;
         }
+
+        return false;
     }
 
     /**
